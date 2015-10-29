360874947bf00a364707b361122968c43f5b2605
Szymon Danielczyk <danielczyk.szymon@gmail.com>
2015-10-29 12:17:24 +0000

    add shipped version of node to PATH because sync_request module require installed node

--- src/server/bin/kibi.bat
+++ src/server/bin/kibi.bat
@@ -17,6 +17,8 @@ TITLE Kibi Server @@version
 set JAVA_POST_INSTALL=%DIR%\src\node_modules\jdbc\node_modules\java\postInstall.js
 "%NODE%" "%JAVA_POST_INSTALL%"
 
+REM add shipped node to the path as sync_request in fallback mode requires installed node
+set PATH=%PATH%;%DIR%\node
 
 "%NODE%" "%SERVER%" %*
 

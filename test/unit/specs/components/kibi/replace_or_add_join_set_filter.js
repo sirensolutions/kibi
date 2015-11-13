9306e070d6106e6cf263f17a7ef5e63a55694ad5
Stéphane Campinas <stephane.campinas@gmail.com>
2015-11-13 09:59:36 +0000

    updated tests #382

--- test/unit/specs/components/kibi/replace_or_add_join_set_filter.js
+++ test/unit/specs/components/kibi/replace_or_add_join_set_filter.js
@@ -1,10 +1,10 @@
 define(function (require) {
 
-  var replace_or_add_join_filter = require('components/sindicetech/join_filter_helper/lib/replace_or_add_join_filter');
+  var replace_or_add_join_set_filter = require('components/sindicetech/join_filter_helper/lib/replace_or_add_join_set_filter');
 
   describe('Kibi Components', function () {
 
-    describe('replace_or_add_join_filter', function () {
+    describe('replace_or_add_join_set_filter', function () {
 
       it('add to empty and strip meta', function () {
         var filters = [];
@@ -15,7 +15,7 @@ define(function (require) {
         var expected = [{
           join_set: {}
         }];
-        replace_or_add_join_filter(filters, joinFilter, true);
+        replace_or_add_join_set_filter(filters, joinFilter, true);
         expect(filters).to.eql(expected);
       });
 
@@ -29,7 +29,7 @@ define(function (require) {
           join_set: {},
           meta: {}
         }];
-        replace_or_add_join_filter(filters, joinFilter, false);
+        replace_or_add_join_set_filter(filters, joinFilter, false);
         expect(filters).to.eql(expected);
       });
 
@@ -45,7 +45,7 @@ define(function (require) {
 
         var filters = [filter1];
         var expected = [filter2];
-        replace_or_add_join_filter(filters, filter2, false);
+        replace_or_add_join_set_filter(filters, filter2, false);
         expect(filters).to.eql(expected);
       });
 
@@ -63,7 +63,7 @@ define(function (require) {
         var expected = [{
           join_set:{indexes:['index2']}
         }];
-        replace_or_add_join_filter(filters, filter2, true);
+        replace_or_add_join_set_filter(filters, filter2, true);
         expect(filters).to.eql(expected);
       });
 

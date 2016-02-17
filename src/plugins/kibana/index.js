7b4149dbb5514b40f0c3c802297d594f7fd2929c
szydan <danielczyk.szymon@gmail.com>
2016-02-17 15:46:34 +0000

    change kibana app title to Kibi

--- src/plugins/kibana/index.js
+++ src/plugins/kibana/index.js
@@ -11,7 +11,7 @@ module.exports = function (kibana) {
 
     uiExports: {
       app: {
-        title: 'Kibana',
+        title: 'Kibi',
         description: 'the kibana you know and love',
         //icon: 'plugins/kibana/settings/sections/about/barcode.svg',
         main: 'plugins/kibana/kibana',

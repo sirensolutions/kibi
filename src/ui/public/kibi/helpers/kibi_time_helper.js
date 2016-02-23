a1a46e2aca606df89e5c51d5b5ed9a90cd39a6cf
szydan <danielczyk.szymon@gmail.com>
2016-02-23 16:53:49 +0000

    refactored kibi_time_helper
    Now it is using savedDashboards.find to minimise the number of requests

--- src/ui/public/kibi/helpers/kibi_time_helper.js
+++ src/ui/public/kibi/helpers/kibi_time_helper.js
@@ -12,28 +12,39 @@ define(function (require) {
     }
 
     KibiTimeHelper.prototype.updateTimeFilterForDashboard = function (dashboardId, timeRangeF) {
-      return new Promise(function (fulfill, reject) {
-        savedDashboards.get(dashboardId).then(function (savedDashboard) {
-          var timeRangeFilter = _.cloneDeep(timeRangeF);
-          // check if timeRestore is set to true and only in this case try to modify the timeFilter
-          if (savedDashboard.timeRestore === true && savedDashboard.timeFrom && savedDashboard.timeTo) {
-            // here take this values from kibiState
-            // if there is no value in kibi state then it will already have correct time taken from global time
-            var time = kibiStateHelper.getTimeForDashboardId(dashboardId);
-            if (time) {
-              var from = datemath.parseWithPrecision(time.from, false, $rootScope.kibiTimePrecision).valueOf();
-              var to = datemath.parseWithPrecision(time.to, false, $rootScope.kibiTimePrecision).valueOf();
-              if (Object.keys(timeRangeFilter.range).length === 1) {
-                timeRangeFilter.range[Object.keys(timeRangeFilter.range)[0]].gte = from;
-                timeRangeFilter.range[Object.keys(timeRangeFilter.range)[0]].lte = to;
-              }
+
+      var updateTimeRangeFilter = function (dashboardBean, timeRangeFilter) {
+        // check if timeRestore is set to true and only in this case try to modify the timeFilter
+        if (dashboardBean && dashboardBean.timeRestore === true && dashboardBean.timeFrom && dashboardBean.timeTo) {
+          // here take this values from kibiState
+          // if there is no value in kibi state then it will already have correct time taken from global time
+          var time = kibiStateHelper.getTimeForDashboardId(dashboardBean.id);
+          if (time) {
+            var from = datemath.parseWithPrecision(time.from, false, $rootScope.kibiTimePrecision).valueOf();
+            var to = datemath.parseWithPrecision(time.to, false, $rootScope.kibiTimePrecision).valueOf();
+            if (Object.keys(timeRangeFilter.range).length === 1) {
+              timeRangeFilter.range[Object.keys(timeRangeFilter.range)[0]].gte = from;
+              timeRangeFilter.range[Object.keys(timeRangeFilter.range)[0]].lte = to;
             }
           }
-          fulfill(timeRangeFilter);
-        }).catch(function (err) {
-          reject(err);
+        }
+      };
+
+      var timeRangeFilter = _.cloneDeep(timeRangeF);
+
+      // here use find as the results are already cached
+      return savedDashboards.find().then(function (dashboardsResp) {
+        var dashboardBean = _.find(dashboardsResp.hits, function (hit) {
+          return hit.id === dashboardId;
         });
+        if (dashboardBean === undefined) {
+          return Promise.reject(new Error('Could not find a dashboard with id: ' + dashboardId));
+        }
+
+        updateTimeRangeFilter(dashboardBean, timeRangeFilter);
+        return timeRangeFilter;
       });
+
     };
 
     return new KibiTimeHelper();

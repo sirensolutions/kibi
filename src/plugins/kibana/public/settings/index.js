e68dc10dd2324e10a8f9d6c695f56a63f9996224
szydan <danielczyk.szymon@gmail.com>
2016-03-22 14:22:38 +0000

    removed commented code

--- src/plugins/kibana/public/settings/index.js
+++ src/plugins/kibana/public/settings/index.js
@@ -28,20 +28,11 @@ uiRoutes
   redirectTo: '/settings/indices'
 });
 
- // kibi: register objects used by kibi
- //  var savedObjectRegistry = require('ui/saved_objects/saved_object_registry');
- //  savedObjectRegistry.register(require('plugins/kibana/settings/sections/kibi_datasources/services/saved_datasource_register'));
- //  savedObjectRegistry.register(require('plugins/kibana/settings/sections/kibi_queries/services/saved_query_register'));
- //  savedObjectRegistry.register(require('plugins/kibana/settings/sections/kibi_templates/services/saved_templates_register'));
- //  savedObjectRegistry.register(require('plugins/kibana/settings/sections/kibi_dashboard_groups/services/saved_dashboard_groups_register'));
- // // kibi: end
-
 require('ui/index_patterns/routeSetup/loadDefault')({
   notRequiredRe: /^\/settings\//,
   whenMissingRedirectTo: '/settings/indices'
 });
 
-  //require('ui/modules')
 uiModules
 .get('apps/settings')
 .directive('kbnSettingsApp', function (Private, $route, timefilter) {

3b5e790240eff9595f004fc955e97bc28b99f407
szydan <danielczyk.szymon@gmail.com>
2016-03-29 15:29:55 +0100

    added kibi_session_helper

---
+++ src/ui/public/kibi/helpers/kibi_state_helper/services/_saved_session.js
@@ -0,0 +1,42 @@
+define(function (require) {
+
+  var module = require('ui/modules').get('ui/kibi/helpers/kibi_state_helper/services/saved_sessions', []);
+  var angular = require('angular');
+  var _ = require('lodash');
+
+  module.factory('SavedSession', function (courier) {
+    _.class(SavedSession).inherits(courier.SavedObject);
+
+    function SavedSession(id) {
+
+      SavedSession.Super.call(this, {
+        id: id,
+        type: SavedSession.type,
+        mapping: SavedSession.mapping,
+        defaults: {
+          title: 'New Session',
+          description: '',
+          session_data: {},
+          version: 1,
+          timeCreated: undefined,
+          timeUpdated: undefined
+        }
+      });
+    }
+
+    // save these objects with the 'dashboard' type
+    SavedSession.type = 'session';
+
+    // if type:dashboard has no mapping, we push this mapping into ES
+    SavedSession.mapping = {
+      title: 'string',
+      description: 'string',
+      session_data: 'json',
+      version: 'integer',
+      timeCreated: 'date',
+      timeUpdated: 'date'
+    };
+
+    return SavedSession;
+  });
+});
--- webpackShims/angular.js
+++ webpackShims/angular.js
@@ -3,5 +3,6 @@ require('node_modules/angular/angular');
 module.exports = window.angular;
 
 require('node_modules/angular-elastic/elastic');
+require('node_modules/angular-cookies/angular-cookies');
 
-require('ui/modules').get('kibana', ['monospaced.elastic']);
+require('ui/modules').get('kibana', ['monospaced.elastic', 'ngCookies']);

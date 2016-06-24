53f8c4e83af6d09c4b5599ad6a394abca47e5391
szydan <danielczyk.szymon@gmail.com>
2016-06-24 16:56:07 +0100

    Added note about Boom upgrade

--- src/plugins/kibi_core/index.js
+++ src/plugins/kibi_core/index.js
@@ -60,6 +60,9 @@ module.exports = function (kibana) {
         err = Boom.wrap(error, 400);
       } else {
         err = Boom.badRequest('Failed to execute query on an external datasource ' + JSON.stringify(error));
+        // TODO: review and check that the line below works after migrating to new Boom version
+        // https://github.com/sirensolutions/kibi-internal/issues/1180
+        //err = Boom.badRequest('Failed to execute query on an external datasource', error);
       }
       return reply(err);
     });

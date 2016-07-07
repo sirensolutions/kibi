c4bb8b209a5db25960c20fee49105cecbdea29c0
Blakko <fabio.tacchelli@gmail.com>
2016-07-07 10:29:59 +0200

    Add tests

--- src/server/gremlin_server/__tests__/gremlin_server.js
+++ src/server/gremlin_server/__tests__/gremlin_server.js
@@ -12,7 +12,7 @@ describe('Kibi Gremlin Server', function () {
     gremlin = new GremlinServerHandler(server);
   });
 
-  it('should pass the Java 8 check - single string', async function () {
+  it('should pass the Java 8 check (Oracle)- single string', async function () {
     const javaVersion =
         'java version "1.8.0_25"' + JSON.stringify(os.EOL)
       + 'Java(TM) SE Runtime Environment (build 1.8.0_25-b17)' + JSON.stringify(os.EOL)
@@ -23,10 +23,36 @@ describe('Kibi Gremlin Server', function () {
     expect(ret.v).to.be(true);
   });
 
-  it('should pass the Java 8 check - multiple strings', async function () {
+  it('should pass the Java 8 check (Oracle)- multiple strings', async function () {
     const javaVersion = [
       'java version "1.8.0_25"' + JSON.stringify(os.EOL),
       'Java(TM) SE Runtime Environment (build 1.8.0_25-b17)' + JSON.stringify(os.EOL),
+      'Java HotSpot(TM) 64-Bit Server VM (build 25.25-b02, mixed mode)'
+    ];
+
+    let ret = gremlin._checkJavaVersionString(javaVersion[0]);
+    expect(ret.v).to.be(true);
+    ret = gremlin._checkJavaVersionString(javaVersion[1]);
+    expect(ret).to.be(null);
+    ret = gremlin._checkJavaVersionString(javaVersion[2]);
+    expect(ret).to.be(null);
+  });
+
+  it('should pass the Java 8 (OpenJDK) check - single string', async function () {
+    const javaVersion =
+        'openjdk version "1.8.0_25"' + JSON.stringify(os.EOL)
+      + 'OpenJDK Runtime Environment (build 1.8.0_25-8u25-b17-0ubuntu4-14.04-b14)' + JSON.stringify(os.EOL)
+      + 'OpenJDK 64-Bit Server VM (build 25.25-b02, mixed mode)';
+
+    const ret = gremlin._checkJavaVersionString(javaVersion);
+
+    expect(ret.v).to.be(true);
+  });
+
+  it('should pass the Java 8 (OpenJDK) check - multiple strings', async function () {
+    const javaVersion = [
+      'openjdk version "1.8.0_25"' + JSON.stringify(os.EOL),
+      'OpenJDK Runtime Environment (build 1.8.0_25-8u25-b17-0ubuntu4-14.04-b14)' + JSON.stringify(os.EOL),
       'Java HotSpot(TM) 64-Bit Server VM (build 25.25-b02, mixed mode)'
     ];
 

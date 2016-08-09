fca5e2153a096ed1e4a022711786d464a4678cae
Stéphane Campinas <stephane.campinas@gmail.com>
2016-08-09 10:33:31 +0100

    use kibiutils query dependent method in _should_entity_uri_be_enabled

--- src/ui/public/kibi/components/commons/__tests__/should_entity_uri_be_enabled.js
+++ src/ui/public/kibi/components/commons/__tests__/should_entity_uri_be_enabled.js
@@ -1,11 +1,11 @@
-var ngMock = require('ngMock');
-var shouldEntityUriBeEnabled;
-var expect = require('expect.js');
-var _ = require('lodash');
+const ngMock = require('ngMock');
+let shouldEntityUriBeEnabled;
+const expect = require('expect.js');
+const _ = require('lodash');
+const mockSavedObjects = require('fixtures/kibi/mock_saved_objects');
 
 describe('Kibi Components', function () {
   describe('Commons', function () {
-
 
     describe('_should_entity_uri_be_enabled use query ids', function () {
 
@@ -15,67 +15,78 @@ describe('Kibi Components', function () {
         ngMock.module('kibana');
 
         ngMock.module('queries_editor/services/saved_queries', function ($provide) {
-          $provide.service('savedQueries', function (Promise) {
-            const queries = {
-              hits: [
-                {
-                  id: 'goaway',
-                  resultQuery: 'select name from person',
-                  activationQuery: 'select name from person'
-                },
-                {
-                  id: 'sql-result-query',
-                  resultQuery: 'select name from table where id = @doc[_source][id]@'
-                },
-                {
-                  id: 'sparql-result-query',
-                  resultQuery: 'select ?name { <@doc[_source][id]@> :name ?name }'
-                },
-                {
-                  id: 'sql-activation-query',
-                  activationQuery: 'select name from table where id = @doc[_source][id]@'
-                },
-                {
-                  id: 'sparql-activation-query',
-                  resultQuery: 'ask { <@doc[_source][id]@> :name ?name }'
-                },
-                {
-                  id: 'rest-body',
-                  rest_body: '@doc[_source][id]@'
-                },
-                {
-                  id: 'rest-params',
-                  rest_params: [ { value: '@doc[_source][id]@' } ]
-                },
-                {
-                  id: 'rest-headers',
-                  rest_headers: [ { value: '@doc[_source][id]@' } ]
-                },
-                {
-                  id: 'rest-path',
-                  rest_path: '/users/@doc[_source][user]@'
-                }
-              ]
-            };
-            return {
-              find: function () {
-                return Promise.resolve(queries);
-              },
-              get: function (id) {
-                var query = _.find(queries.hits, 'id', id);
-
-                if (!query) {
-                  return Promise.reject('What is this id? ' + id);
-                }
-                return Promise.resolve(query);
-              }
-            };
-          });
+          $provide.service('savedQueries', (Promise) => mockSavedObjects(Promise)('savedQueries', [
+            {
+              id: 'goaway',
+              resultQuery: 'select name from person',
+              activationQuery: 'select name from person'
+            },
+            {
+              id: 'sql-query-comment-depends',
+              resultQuery: `select name
+                            from table
+                            /* where id = @doc[_source][name]@ */
+                            where id = @doc[_source][id]@`
+            },
+            {
+              id: 'sql-query-comment-depends-not',
+              resultQuery: `select name
+                            from table
+                            /* where id = @doc[_source][name]@ */
+                            where id = 123`
+            },
+            {
+              id: 'sql-result-query',
+              resultQuery: 'select name from table where id = @doc[_source][id]@'
+            },
+            {
+              id: 'sparql-result-query',
+              resultQuery: 'select ?name { <@doc[_source][id]@> :name ?name }'
+            },
+            {
+              id: 'sql-activation-query',
+              activationQuery: 'select name from table where id = @doc[_source][id]@'
+            },
+            {
+              id: 'sparql-activation-query',
+              resultQuery: 'ask { <@doc[_source][id]@> :name ?name }'
+            },
+            {
+              id: 'rest-body',
+              rest_body: '@doc[_source][id]@'
+            },
+            {
+              id: 'rest-params',
+              rest_params: [ { value: '@doc[_source][id]@' } ]
+            },
+            {
+              id: 'rest-headers',
+              rest_headers: [ { value: '@doc[_source][id]@' } ]
+            },
+            {
+              id: 'rest-path',
+              rest_path: '/users/@doc[_source][user]@'
+            }
+          ]));
         });
 
         ngMock.inject(function (Private) {
           shouldEntityUriBeEnabled = Private(require('ui/kibi/components/commons/_should_entity_uri_be_enabled'));
         });
+      });
+
+      it('should not require entity that is commented', function (done) {
+        shouldEntityUriBeEnabled([ 'sql-query-comment-depends-not' ]).then(function (required) {
+          expect(required).to.be(false);
+          done();
+        }).catch(done);
+      });
+
+      it('should require entity that is not commented', function (done) {
+        shouldEntityUriBeEnabled([ 'sql-query-comment-depends' ]).then(function (required) {
+          expect(required).to.be(true);
+          done();
+        }).catch(done);
       });
 
       it('should not be required', function (done) {
@@ -130,49 +141,6 @@ describe('Kibi Components', function () {
       it('should be required for REST path', function (done) {
         shouldEntityUriBeEnabled([ 'rest-path' ]).then(function (required) {
           expect(required).to.be(true);
-          done();
-        }).catch(done);
-      });
-
-    });
-
-
-    describe('_should_entity_uri_be_enabled use queries', function () {
-
-      require('testUtils/noDigestPromises').activateForSuite();
-
-      beforeEach(function () {
-        ngMock.module('kibana');
-
-        ngMock.inject(function (Private) {
-          shouldEntityUriBeEnabled = Private(require('ui/kibi/components/commons/_should_entity_uri_be_enabled'));
-        });
-      });
-
-      it('single query should return true', function (done) {
-
-        var query = {
-          rest_params: [
-            {name: 'param1', value: '@doc[_source][id]@'}
-          ]
-        };
-
-        shouldEntityUriBeEnabled(null, [query]).then(function (required) {
-          expect(required).to.be(true);
-          done();
-        }).catch(done);
-
-      });
-
-      it('single query should return false', function (done) {
-        var query = {
-          rest_params: [
-            {name: 'param1', value: 'id'}
-          ]
-        };
-
-        shouldEntityUriBeEnabled(null, [ 'select * from table1 where 1 limit 10' ]).then(function (required) {
-          expect(required).to.be(false);
           done();
         }).catch(done);
       });

9e99b37fcbbef777dfd857fde45f490851c03d34
mehmetfurkanaydin <aydin.mehmetfurkan@gmail.com>
2016-08-10 17:19:01 +0100

    datasources_schema.js file changed

--- src/plugins/kibi_core/lib/datasources_schema.js
+++ src/plugins/kibi_core/lib/datasources_schema.js
@@ -1,10 +1,8 @@
 var fs = require('fs');
 var path = require('path');
-// TODO: consider to load it from config folder
-//var env = process.env.NODE_ENV || 'development';
-//var datasourceSchemaPath =
-//  process.env.CONFIG_PATH ?
-//  process.env.CONFIG_PATH.replace(/kibi\.yml/, 'datasources-schema.json') :
-//  path.join(__dirname, 'datasources-schema.json');
-var datasourceSchemaPath = path.join(__dirname, 'datasources_schema.json');
+var env = process.env.NODE_ENV || 'development';
+var datasourceSchemaPath =
+  process.env.KIBI_SCHEMA_PATH ?
+  process.env.KIBI_SCHEMA_PATH :
+  path.join(__dirname, 'datasources_schema.json');
 module.exports = JSON.parse(fs.readFileSync(datasourceSchemaPath, 'utf8'));

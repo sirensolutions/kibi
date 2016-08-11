fbed5897d585c626383a92bd148cbe00c06f0492
mehmetfurkanaydin <aydin.mehmetfurkan@gmail.com>
2016-08-11 12:40:38 +0100

    Checked, TODO removed

--- src/ui/public/kibi/kibi_doc_table/kibi_doc_table.js
+++ src/ui/public/kibi/kibi_doc_table/kibi_doc_table.js
@@ -63,24 +63,6 @@ define(function (require) {
           $scope.limit += 50;
         };
 
-        // TODO is this needed ?
-        //// This exists to fix the problem of an empty initial column list not playing nice with watchCollection.
-        //$scope.$watch('columns', function (columns) {
-          //if (columns.length !== 0) return;
-
-          //var $state = getAppState();
-          //$scope.columns.push('_source');
-          //if ($state) $state.replace();
-        //});
-
-        //$scope.$watchCollection('columns', function (columns, oldColumns) {
-          //if (oldColumns.length === 1 && oldColumns[0] === '_source' && $scope.columns.length > 1) {
-            //_.pull($scope.columns, '_source');
-          //}
-
-          //if ($scope.columns.length === 0) $scope.columns.push('_source');
-        //});
-
         // kibi: increase the number of results retrieved
         const sampleSize = config.get('discover:sampleSize');
         try {

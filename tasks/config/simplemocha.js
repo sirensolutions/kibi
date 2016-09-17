9423b8e225c0a3fa6a56c0c490ad2e655b02c293
Fabio Corneti <info@corneti.com>
2016-09-17 11:59:17 +0100

    Support grep and execution of mocha tests in installedPlugins

--- tasks/config/simplemocha.js
+++ tasks/config/simplemocha.js
@@ -1,17 +1,21 @@
-module.exports = {
+module.exports = (grunt) => ({
   options: {
     timeout: 60000, // kibi: increased default timeout for jenkins build
     slow: 5000,
     ignoreLeaks: false,
-    reporter: 'list'
+    reporter: 'list',
+    grep: grunt.option('grep')
   },
   all: {
     src: [
       'test/**/__tests__/**/*.js',
       'src/**/__tests__/**/*.js',
+      'installedPlugins/**/lib/**/__tests__/**/*.js',
       'test/fixtures/__tests__/*.js',
       '!src/**/public/**',
-      '!src/ui/**'
+      '!src/ui/**',
+      '!installedPlugins/**/public/**',
+      '!installedPlugins/**/node_modules/**',
     ]
   }
-};
+});

31a7727b592f79a534da29a7c06d779beb71cc19
Fabio Corneti <info@corneti.com>
2016-11-01 13:52:26 +0000

    Functional tests for session API

---
+++ src/plugins/saved_objects_api/lib/model/__tests__/functional/scenarios/empty/index_data.js
@@ -0,0 +1,20 @@
+import requirefrom from 'requirefrom';
+const fromRoot = requirefrom('src/utils')('fromRoot');
+const packageJson = require(fromRoot('package.json'));
+
+/**
+ * An empty .kibi index.
+ */
+export default [
+  {
+    'index': {
+      '_index': '.kibi',
+      '_type': 'config',
+      '_id': packageJson.kibi_version
+    }
+  },
+  {
+    'buildNum': packageJson.build.number,
+    'dateFormat:tz': 'UTC'
+  }
+];

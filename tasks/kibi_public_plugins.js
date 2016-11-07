1b7ca931df5d47d64459a3fac5f7069e7380746b
szydan <danielczyk.szymon@gmail.com>
2016-11-07 11:32:59 +0000

    updated kibi timeline to version 4.5.4

--- tasks/kibi_public_plugins.js
+++ tasks/kibi_public_plugins.js
@@ -16,7 +16,7 @@ module.exports = function (grunt) {
       dest: '/tmp/kibi_wordcloud_vis.zip'
     },
     {
-      url: 'https://github.com/sirensolutions/kibi_timeline_vis/raw/4.5.3-2/target/kibi_timeline_vis-4.5.3-2.zip',
+      url: 'https://github.com/sirensolutions/kibi_timeline_vis/raw/4.5.4/target/kibi_timeline_vis-4.5.4.zip',
       dest: '/tmp/kibi_timeline_vis.zip'
     },
     {

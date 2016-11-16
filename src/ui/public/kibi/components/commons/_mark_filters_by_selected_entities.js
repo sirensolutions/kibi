73fa1ee0286dc090368a191ebc17bfda34c24258
Stéphane Campinas <stephane.campinas@gmail.com>
2016-11-16 11:26:33 +0000

    check if we are on the visualize or dashboard tab before trying to get the selected entity

--- src/ui/public/kibi/components/commons/_mark_filters_by_selected_entities.js
+++ src/ui/public/kibi/components/commons/_mark_filters_by_selected_entities.js
@@ -1,25 +1,25 @@
 define(function (require) {
-
   return function MarkFiltersBySelectedEntitiesFactory(Promise, Private, kibiState) {
-
-    var _ = require('lodash');
-    var _shouldEntityURIBeEnabled = Private(require('ui/kibi/components/commons/_should_entity_uri_be_enabled'));
+    const urlHelper = Private(require('ui/kibi/helpers/url_helper'));
+    const _ = require('lodash');
+    const _shouldEntityURIBeEnabled = Private(require('ui/kibi/components/commons/_should_entity_uri_be_enabled'));
 
     return function (filters) {
-      var promises = [];
-      _.each(filters, function (filter) {
+      const promises = _.map(filters, filter => {
         if (filter.dbfilter) {
-          promises.push(_shouldEntityURIBeEnabled([filter.dbfilter.queryid]));
-        } else {
-          promises.push(Promise.resolve(false));
+          return _shouldEntityURIBeEnabled([filter.dbfilter.queryid]);
         }
+        return Promise.resolve(false);
       });
 
       return Promise.all(promises).then(function (results) {
         _.each(results, function (res, index) {
           filters[index].meta.dependsOnSelectedEntities = res;
           filters[index].meta.dependsOnSelectedEntitiesDisabled = res && kibiState.isSelectedEntityDisabled();
-          filters[index].meta.markDependOnSelectedEntities = Boolean(kibiState.getEntityURI());
+          filters[index].meta.markDependOnSelectedEntities = false;
+          if (urlHelper.onVisualizeTab() || urlHelper.onSettingsTab()) {
+            filters[index].meta.markDependOnSelectedEntities = Boolean(kibiState.getEntityURI());
+          }
         });
         return filters;
       });

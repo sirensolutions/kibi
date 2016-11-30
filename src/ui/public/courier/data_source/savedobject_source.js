0c3d5f3e509e224a4366031f3280e450c887b09f
Fabio Corneti <info@corneti.com>
2016-11-30 02:44:04 +0000

    Created a courier data_source for saved objects managed by the API

---
+++ src/ui/public/courier/data_source/savedobject_source.js
@@ -0,0 +1,36 @@
+define(function (require) {
+  let _ = require('lodash');
+
+  return function SavedObjectSourceFactory(Private) {
+    let DocSource = Private(require('ui/courier/data_source/doc_source'));
+    let SavedObjectRequest = Private(require('ui/courier/fetch/request/savedobject'));
+    let SavedObjectStrategy = Private(require('ui/courier/fetch/strategy/savedobject'));
+
+    _.class(SavedObjectSource).inherits(DocSource);
+    function SavedObjectSource(initialState) {
+      SavedObjectSource.Super.call(this, initialState, SavedObjectStrategy);
+    }
+
+    /*****
+     * PUBLIC API
+     *****/
+
+    SavedObjectSource.prototype._createRequest = function (defer) {
+      return new SavedObjectRequest(this, defer);
+    };
+
+    /*****
+     * PRIVATE API
+     *****/
+
+    /**
+     * Get the type of this SourceAbstract
+     * @return {string} - 'savedobject'
+     */
+    SavedObjectSource.prototype._getType = function () {
+      return 'savedobject';
+    };
+
+    return SavedObjectSource;
+  };
+});

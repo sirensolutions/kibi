002fa8a6250a52d9e69633f983a9666b2252b612
Fabio Corneti <info@corneti.com>
2016-12-03 11:04:00 +0000

    Added server coverage and moved browser coverage to a separate directory

---
+++ tasks/config/mocha_istanbul.js
@@ -0,0 +1,34 @@
+module.exports = (grunt) => ({
+  options: {
+    timeout: 60000,
+    slow: 5000,
+    grep: grunt.option('grep'),
+    mochaOptions: [
+      '--compilers',
+      'js:babel-core/register',
+      '--check-leaks'
+    ],
+    istanbulOptions: [
+      '--harmony',
+      '--handle-sigint'
+    ],
+    coverageFolder: 'coverage/server',
+    reportFormats: [
+      'cobertura',
+      'lcov',
+      'text-summary'
+    ]
+  },
+  all: {
+    src: [
+      'test/**/__tests__/**/*.js',
+      'src/**/__tests__/**/*.js',
+      'installedPlugins/**/lib/**/__tests__/**/*.js',
+      'test/fixtures/__tests__/*.js',
+      '!src/**/public/**',
+      '!src/ui/**',
+      '!installedPlugins/**/public/**',
+      '!installedPlugins/**/node_modules/**',
+    ]
+  }
+});

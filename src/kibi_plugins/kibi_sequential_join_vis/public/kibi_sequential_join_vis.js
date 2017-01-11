ea5710f6d97b50d07bc0f2d2c03bfbd915c2c08f
Stéphane Campinas <stephane.campinas@gmail.com>
2017-01-11 12:57:55 +0000

    ported kibi sequential join vis to use the import syntax

--- src/kibi_plugins/kibi_sequential_join_vis/public/kibi_sequential_join_vis.js
+++ src/kibi_plugins/kibi_sequential_join_vis/public/kibi_sequential_join_vis.js
@@ -1,38 +1,38 @@
-define(function (require) {
-  // we need to load the css ourselves
-  require('plugins/kibi_sequential_join_vis/kibi_sequential_join_vis.less');
+import templateVisTypeProvider from 'ui/template_vis_type/template_vis_type';
+// we need to load the css ourselves
+import 'plugins/kibi_sequential_join_vis/kibi_sequential_join_vis.less';
+// we also need to load the controller and used by the template
+import 'plugins/kibi_sequential_join_vis/kibi_sequential_join_vis_controller';
+// our params are a bit complex so we will manage them with a directive
+import 'plugins/kibi_sequential_join_vis/kibi_sequential_join_vis_params';
+// register the provider with the visTypes registry
+import registry from 'ui/registry/vis_types';
+import template from 'plugins/kibi_sequential_join_vis/kibi_sequential_join_vis.html';
 
-  // we also need to load the controller and used by the template
-  require('plugins/kibi_sequential_join_vis/kibi_sequential_join_vis_controller');
+registry.register(KibiSequentialJoinVisTypeProvider);
 
-  // our params are a bit complex so we will manage them with a directive
-  require('plugins/kibi_sequential_join_vis/kibi_sequential_join_vis_params');
+function KibiSequentialJoinVisTypeProvider(Private) {
+  const TemplateVisType = Private(templateVisTypeProvider);
 
-  // register the provider with the visTypes registry
-  require('ui/registry/vis_types').register(KibiSequentialJoinVisTypeProvider);
-
-  function KibiSequentialJoinVisTypeProvider(Private) {
-    const TemplateVisType = Private(require('ui/template_vis_type/TemplateVisType'));
-
-    // return the visType object, which kibana will use to display and configure new
-    // Vis object of this type.
-    return new TemplateVisType({
-      name: 'kibi_sequential_join_vis',
-      title: 'Kibi relational filter',
-      icon: 'fa-arrows-h',
-      description: 'Relational widget displays buttons which allow user to switch between dashboards and preserve applied restrictions',
-      template: require('plugins/kibi_sequential_join_vis/kibi_sequential_join_vis.html'),
-      params: {
-        defaults: {
-          buttons: []
-        },
-        editor: '<kibi-sequential-join-vis-params></kibi-sequential-join-vis-params>'
+  // return the visType object, which kibana will use to display and configure new
+  // Vis object of this type.
+  return new TemplateVisType({
+    name: 'kibi_sequential_join_vis',
+    title: 'Kibi relational filter',
+    icon: 'fa-arrows-h',
+    description: 'Relational widget displays buttons which allow user to switch between dashboards and preserve applied restrictions',
+    template,
+    params: {
+      defaults: {
+        buttons: []
       },
-      requiresMultiSearch: true,
-      requiresSearch: false,
-      version: 2
-    });
-  }
+      editor: '<kibi-sequential-join-vis-params></kibi-sequential-join-vis-params>'
+    },
+    requiresMultiSearch: true,
+    requiresSearch: false,
+    version: 2
+  });
+}
 
-  return KibiSequentialJoinVisTypeProvider;
-});
+// export the provider so that the visType can be required with Private()
+export default KibiSequentialJoinVisTypeProvider;

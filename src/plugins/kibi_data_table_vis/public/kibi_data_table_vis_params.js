014613dd7e1a6ba667d5dc8c67c381de292479c5
szydan <danielczyk.szymon@gmail.com>
2017-02-21 13:51:41 +0100

    remove lint warnings in kibi_data_table_vis_params.js

--- src/plugins/kibi_data_table_vis/public/kibi_data_table_vis_params.js
+++ src/plugins/kibi_data_table_vis/public/kibi_data_table_vis_params.js
@@ -1,5 +1,5 @@
 define(function (require) {
-  var _ = require('lodash');
+  const _ = require('lodash');
 
   require('ui/kibi/directives/kibi_select');
   require('ui/kibi/directives/kibi_array_param');
@@ -7,7 +7,7 @@ define(function (require) {
   require('ui/modules').get('kibana/kibi_data_table_vis')
   .directive('kibiDataTableVisParams', function (savedDatasources, $rootScope, $route, createNotifier, $window) {
 
-    var notify = createNotifier({
+    const notify = createNotifier({
       location: 'Enhanced search results'
     });
 
@@ -32,9 +32,9 @@ define(function (require) {
 
         // check if there is any click actions / relational column definition
         // before removing a column of the table
-        var removeRemoveColumnHandler = $rootScope.$on('kibi:remove:column', function (event, column) {
+        const removeRemoveColumnHandler = $rootScope.$on('kibi:remove:column', function (event, column) {
           if (column) {
-            var msg;
+            let msg;
 
             if ($scope.vis.params.enableQueryFields && column.fieldName === $scope.vis.params.queryFieldName) {
               msg = 'Are you sure you want to remove the relational column "' +
@@ -47,7 +47,7 @@ define(function (require) {
               return;
             }
 
-            var clicks = 0;
+            let clicks = 0;
             _.each($scope.vis.params.clickOptions, function (clickOption) {
               if (clickOption.columnField === column.fieldName) {
                 clicks++;
@@ -69,7 +69,7 @@ define(function (require) {
         });
 
         // Point savedObject.columns to vis.params.columns
-        var removeSavedObjectColumnsChangedHandler = $rootScope.$on('kibi:vis:savedObjectColumns-changed',
+        const removeSavedObjectColumnsChangedHandler = $rootScope.$on('kibi:vis:savedObjectColumns-changed',
           function (event, savedObject) {
             if (savedObject && savedObject.columns !== $scope.vis.params.columns) {
               savedObject.columns = $scope.vis.params.columns;
@@ -105,13 +105,13 @@ define(function (require) {
         // Queries
         // =======
 
-        var previousName = null;
+        let previousName = null;
         $scope.$watch('vis.params.queryFieldName', function () {
           if (!$scope.vis.params.queryFieldName) {
             return;
           }
-          var name = $scope.vis.params.queryFieldName;
-          var i = -1;
+          const name = $scope.vis.params.queryFieldName;
+          let i = -1;
           if (previousName) {
             i = $scope.vis.params.columns.indexOf(previousName);
             if (i > -1) {
@@ -129,7 +129,7 @@ define(function (require) {
         $scope.$watch('vis.params.enableQueryFields', function () {
           if (!$scope.vis.params.enableQueryFields) {
             if (previousName) {
-              var i = $scope.vis.params.columns.indexOf(previousName);
+              const i = $scope.vis.params.columns.indexOf(previousName);
               if (i > -1) {
                 $scope.vis.params.columns.splice(i, 1);
               }
@@ -167,7 +167,7 @@ define(function (require) {
         $scope.clickHandlerValidationStates = [];
 
         // Initialise the clicks parameters.
-        var clickHandlersChanged = $scope.clickHandlersChanged = function () {
+        const clickHandlersChanged = $scope.clickHandlersChanged = function () {
           _.each($scope.vis.params.clickOptions, function (clickOption, ind) {
             if (!clickOption.uriFormat && clickOption.type === 'link') {
               clickOption.uriFormat = '@URL@';
@@ -178,7 +178,7 @@ define(function (require) {
         /**
          * Checks if any column has two or more of the same click action
          */
-        var _checkForDuplicates = function () {
+        const _checkForDuplicates = function () {
           $scope.clickHandlerValidationStates.length = 0;
           _.each($scope.vis.params.clickOptions, function (clickOption) {
             $scope.clickHandlerValidationStates.push({
@@ -193,8 +193,8 @@ define(function (require) {
           })
           .each(function (count, key) {
             if (count !== 1) {
-              var parts = key.split('$#%');
-              var msg = 'You cannot have more than one action of the same type on a same column: ' +
+              const parts = key.split('$#%');
+              const msg = 'You cannot have more than one action of the same type on a same column: ' +
                         'found ' + count + ' ' + parts[0] + ' actions on the column ' + parts[1];
               notify.warning(msg);
               _.each($scope.vis.params.clickOptions, function (clickOption, index) {

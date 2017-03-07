d39cbce00aa52a3f2d8d3f4d42f11890576f735b
SergeyBondarenko <sergibondarenko@gmail.com>
2017-03-07 11:51:17 +0100

    Add a directive to filter relations in settings.

--- src/plugins/kibana/public/settings/sections/kibi_relations/controllers/relations.js
+++ src/plugins/kibana/public/settings/sections/kibi_relations/controllers/relations.js
@@ -52,6 +52,60 @@ define(function (require) {
     };
   });
 
+  app.directive('kibiRelationsSearchBar', () => {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        scope.searchRelations = function (mode) {
+          const searchString = scope[`${mode}SearchString`];
+
+          if (!searchString || searchString.length < 2) {
+            for (const relation in scope.relations[mode]) {
+              if (scope.relations[mode].hasOwnProperty(relation)) {
+                scope.relations[mode][relation].hidden = false;
+              }
+            }
+            return;
+          }
+
+          const search = function (obj, searchString) {
+            let result;
+            for (const key in obj) {
+              if (obj.hasOwnProperty(key)) {
+                if (typeof obj[key] === 'object' && obj[key] !== null || _.isArray(obj[key]) && obj[key].length) {
+                  result = search(obj[key], searchString);
+                  if (result) {
+                    return result;
+                  }
+                }
+                if (typeof obj[key] === 'string') {
+                  const found = obj[key].match(new RegExp(searchString, 'gi'));
+                  if (found && found.length) {
+                    return true;
+                  }
+                }
+              }
+            }
+            return result;
+          };
+
+          const result = [];
+
+          for (const relation in scope.relations[mode]) {
+            if (!scope.relations[mode].hasOwnProperty(relation)) {
+              continue;
+            }
+            if (search(scope.relations[mode][relation], searchString)) {
+              scope.relations[mode][relation].hidden = false;
+            } else {
+              scope.relations[mode][relation].hidden = true;
+            }
+          }
+        };
+      }
+    };
+  });
+
   app.directive('kibiDebounce', function ($timeout) {
     return {
       restrict: 'A',

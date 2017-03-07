f8cc8477c055063b9b6a5e3e6731db5d8d3dc186
SergeyBondarenko <sergibondarenko@gmail.com>
2017-03-07 15:09:08 +0100

    Deprecate searchFor filter in favour of kibiRelationsSearchBar directive.

--- src/plugins/kibana/public/settings/sections/kibi_relations/__tests__/relations.js
+++ src/plugins/kibana/public/settings/sections/kibi_relations/__tests__/relations.js
@@ -1717,58 +1717,5 @@ describe('Kibi Settings', function () {
       });
     });
 
-    describe('filters: searchFor', () => {
-      let $filter;
-
-      beforeEach(() => {
-        ngMock.module('apps/settings');
-        ngMock.inject((_$filter_) => {
-          $filter = _$filter_;
-        });
-      });
-
-      it('should search and filter the relations', () => {
-        const searchString = 'art';
-        const relationsA = [
-          {
-            'indices': [
-              {'indexPatternType': '','indexPatternId': 'investor'}
-            ]
-          },
-          {
-            'indices': [
-              {'indexPatternType': '','indexPatternId': 'company'},
-              {'indexPatternType': '','indexPatternId': 'article'}
-            ]
-          }
-        ];
-        const relationsB = [
-          {
-            'indices': [
-              {'indexPatternType': '','indexPatternId': 'investor'}
-            ]
-          },
-          {
-            'indices': [
-              {'indexPatternType': '','indexPatternId': 'article'},
-              {'indexPatternType': '','indexPatternId': 'company'}
-            ]
-          },
-          {
-            'indices': [
-              {'rocket': [
-                {'engine': '', 'computer': [{'cpu': '', 'software': 'artificial intelligence'}]}
-              ]}
-            ]
-          }
-        ];
-
-        const resultsA = $filter('searchFor')(relationsA, searchString);
-        const resultsB = $filter('searchFor')(relationsB, searchString);
-        expect(resultsA).to.have.length(1);
-        expect(resultsB).to.have.length(2);
-      });
-    });
-
   });
 });
--- src/plugins/kibana/public/settings/sections/kibi_relations/controllers/relations.js
+++ src/plugins/kibana/public/settings/sections/kibi_relations/controllers/relations.js
@@ -9,49 +9,6 @@ define(function (require) {
 
   const app = require('ui/modules').get('apps/settings', ['kibana']);
 
-  app.filter('searchFor', () => {
-    return function (relations, searchString) {
-
-      if (!searchString || searchString.length < 3) {
-        return relations;
-      }
-
-      const search = function (obj, searchString) {
-        let result;
-        for (const key in obj) {
-          if (obj.hasOwnProperty(key)) {
-            if (typeof obj[key] === 'object' && obj[key] !== null || _.isArray(obj[key]) && obj[key].length) {
-              result = search(obj[key], searchString);
-              if (result) {
-                return result;
-              }
-            }
-            if (typeof obj[key] === 'string') {
-              const found = obj[key].match(new RegExp(searchString, 'gi'));
-              if (found && found.length) {
-                return true;
-              }
-            }
-          }
-        }
-        return result;
-      };
-
-      const result = [];
-
-      for (const relation in relations) {
-        if (!relations.hasOwnProperty(relation)) {
-          continue;
-        }
-        if (search(relations[relation], searchString)) {
-          result.push(relations[relation]);
-        }
-      }
-
-      return result;
-    };
-  });
-
   app.directive('kibiRelationsSearchBar', () => {
     return {
       restrict: 'A',

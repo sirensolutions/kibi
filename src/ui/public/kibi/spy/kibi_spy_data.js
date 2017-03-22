a3c2c310580cbd5fcbd6b0b0b86206933cf405a9
Stéphane Campinas <stephane.campinas@gmail.com>
2017-03-22 16:58:37 +0000

    take the debug info from the siren join

--- src/ui/public/kibi/spy/kibi_spy_data.js
+++ src/ui/public/kibi/spy/kibi_spy_data.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import _ from 'lodash';
 import chrome from 'ui/chrome';
+import { toJson } from 'ui/utils/aggressive_parse';
 
 export default function KibiSpyDataFactory(Promise, $http) {
   class KibiSpyData {
@@ -45,132 +46,32 @@ export default function KibiSpyDataFactory(Promise, $http) {
       return this.data;
     }
 
-    getFilterjoinStats(item) {
-      const yes = {
-        markup: '<i class="fa fa-check" aria-hidden="true"></i>'
-      };
-      const no = {
-        markup: '<i class="fa fa-times" aria-hidden="true"></i>'
-      };
-
-      return {
-        headers: [
-          {
-            title: 'Source Index',
-            sortable: false
-          },
-          {
-            title: 'Source Type',
-            sortable: false
-          },
-          {
-            title: 'Source Field',
-            sortable: false
-          },
-          {
-            title: 'Target Index',
-            sortable: false
-          },
-          {
-            title: 'Target Type',
-            sortable: false
-          },
-          {
-            title: 'Target Field',
-            sortable: false
-          },
-          {
-            title: 'Number of Terms',
-            sortable: false,
-            info: 'The size of the filter used to compute the join, i.e., the number of terms across all shards used by the filterjoin.'
-          },
-          {
-            title: 'Bytes',
-            sortable: false,
-            info: 'The size in bytes of the filter used to compute the join.'
-          },
-          {
-            title: 'Pruned',
-            sortable: false,
-            info: 'This indicates if the join computation has been pruned based on the maxTermsPerShard limit.'
-          },
-          {
-            title: 'Cached',
-            sortable: false,
-            info: 'This indicates if the join was already computed and cached.'
-          },
-          {
-            title: 'Terms Encoding',
-            sortable: false,
-            info: 'The encoding to use when transferring terms across the network.'
-          },
-          {
-            title: 'OrderBy',
-            sortable: false,
-            info: 'The ordering to use to lookup the maximum number of terms.'
-          },
-          {
-            title: 'MaxTermsPerShard',
-            sortable: false,
-            info: 'The maximum number of terms per shard to lookup.'
-          },
-          {
-            title: 'Duration',
-            sortable: false
-          }
-        ],
-        rows: (function () {
-          const rootIndex = item.index;
-          const rootType = item.type || '*';
-
-          const actions = item.response.coordinate_search.actions;
-          const rows = [];
-          for (let i = actions.length - 1; i >= 0; i--) {
-            const action = actions[i];
-
-            const row = [
-              rootIndex,
-              rootType,
-              action.relations.to.field,
-              JSON.stringify(action.relations.from.indices, null, ' '),
-              action.relations.from.types.length ? JSON.stringify(action.relations.from.types, null, ' ') : '*',
-              action.relations.from.field,
-              action.size,
-              action.size_in_bytes,
-              action.is_pruned ? yes : no,
-              action.cache_hit ? yes : no,
-              action.terms_encoding,
-              action.order_by || 'default',
-              action.max_terms_per_shard || '-1',
-              action.took + 'ms'
-            ];
-
-            if (action.relations.to.indices) {
-              row[0] = JSON.stringify(action.relations.to.indices, null, ' ');
-              row[1] = action.relations.to.types.length ? JSON.stringify(action.relations.to.types, null, ' ') : '*';
-            }
-
-            rows.push(row);
-          }
-          return rows;
-        }())
-      };
-    }
-
-    hasFilterjoinStats(item) {
-      return item.response.coordinate_search && item.response.coordinate_search.actions.length;
-    }
-
-    translateQueries() {
-      const promises = _.map(this.data, (item) => {
-        return $http.post(chrome.getBasePath() + '/translateToES', { query: angular.toJson(item.query) });
+    getDebugData() {
+      const translations = _.map(this.data, (item) => {
+        return $http.post(chrome.getBasePath() + '/translateToES', { query: toJson(item.query, angular.toJson) });
       });
-      return Promise.all(promises)
-        .then((translateQueries) => {
-          _.each(translateQueries, (q, i) => {
-            this.data[i].translatedQuery = q.data.translatedQuery;
-          });
+      const debugActions = _.map(this.data, (item) => {
+        const debugQuery = item.query;
+        debugQuery.size = 0;
+
+        let request;
+        if (item.type) {
+          request = `${chrome.getBasePath()}/elasticsearch/${item.index}/${item.type}/_search?debug=true`;
+        } else {
+          request = `${chrome.getBasePath()}/elasticsearch/${item.index}/_search?debug=true`;
+        }
+        return $http.post(request, toJson(debugQuery, angular.toJson));
+      });
+      return Promise.all([ ...translations, ...debugActions ])
+      .then(results => {
+        _.each(results, (result, i) => {
+          if (i >= this.data.length) {
+            this.data[i - this.data.length].actions = result.data.actions;
+          } else {
+            this.data[i].translatedQuery = result.data.translatedQuery;
+          }
         });
+      });
     };
   }
 

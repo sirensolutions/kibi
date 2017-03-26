c75cce3b8448f0051c945009324d62cb2680604f
Stéphane Campinas <stephane.campinas@gmail.com>
2017-03-26 10:22:56 +0100

    corrected delete helper tests

--- src/ui/public/kibi/helpers/__tests__/delete_helper.js
+++ src/ui/public/kibi/helpers/__tests__/delete_helper.js
@@ -2,6 +2,7 @@ import noDigestPromises from 'test_utils/no_digest_promises';
 import DeleteHelperProvider from 'ui/kibi/helpers/delete_helper';
 import expect from 'expect.js';
 import ngMock from 'ng_mock';
+import { intersection } from 'lodash';
 import mockSavedObjects from 'fixtures/kibi/mock_saved_objects';
 import sinon from 'auto-release-sinon';
 
@@ -53,6 +54,8 @@ const fakeSavedDashboardGroups = [
   }
 ];
 let deleteHelper;
+let computeGroupsStub;
+let $window;
 
 describe('Kibi Components', function () {
   describe('deleteHelper', function () {
@@ -75,86 +78,102 @@ describe('Kibi Components', function () {
         });
       });
 
-      ngMock.inject(function (Private) {
+      ngMock.inject(function (_$window_, dashboardGroups, Private, Promise) {
+        $window = _$window_;
         deleteHelper = Private(DeleteHelperProvider);
+
+        computeGroupsStub = sinon.stub(dashboardGroups, 'computeGroups').returns(Promise.resolve('computed groups'));
+
+        const _matchInArray = sinon.match(value => intersection([ 'Companies', 'Articles' ], value).length, 'match in array');
+        const getGroupIdsStub = sinon.stub(dashboardGroups, 'getIdsOfDashboardGroupsTheseDashboardsBelongTo');
+        getGroupIdsStub.withArgs(_matchInArray).returns([ 'group-1' ]);
+        getGroupIdsStub.withArgs(sinon.match.any).returns([]);
       });
     });
 
     noDigestPromises.activateForSuite();
 
     describe('getVisualisations', function () {
-      it('should return the visualisation that use query 456', function (done) {
-        deleteHelper._getVisualisations([ '456', '789' ]).then(function (visData) {
+      it('should return the visualisation that use query 456', function () {
+        return deleteHelper._getVisualisations([ '456', '789' ]).then(function (visData) {
           expect(visData[0]).to.eql([ '456' ]);
           expect(visData[1]).to.have.length(1);
           expect(visData[1][0].title).to.be('myvis2');
-          done();
-        }).catch(done);
+        });
       });
 
-      it('should return the visualisations that use queries 123 and 456', function (done) {
-        deleteHelper._getVisualisations([ '456', '123' ]).then(function (visData) {
+      it('should return the visualisations that use queries 123 and 456', function () {
+        return deleteHelper._getVisualisations([ '456', '123' ]).then(function (visData) {
           expect(visData[0]).to.have.length(2);
           expect(visData[0]).to.contain('123');
           expect(visData[0]).to.contain('456');
           expect(visData[1]).to.have.length(2);
           expect(visData[1][0].title).to.be('myvis1');
           expect(visData[1][1].title).to.be('myvis2');
-          done();
-        }).catch(done);
+        });
       });
 
-      it('should return no visualisation', function (done) {
-        deleteHelper._getVisualisations([ '666' ]).then(function (visData) {
+      it('should return no visualisation', function () {
+        return deleteHelper._getVisualisations([ '666' ]).then(function (visData) {
           expect(visData[0]).to.have.length(0);
           expect(visData[1]).to.have.length(0);
-          done();
-        }).catch(done);
+        });
       });
     });
 
     it('should call the delegated delete method method if the service is neither a query nor a dashboard', function () {
-      const spy = sinon.spy();
+      const deleteSpy = sinon.spy();
 
-      deleteHelper.deleteByType('aaa', null, spy);
-      expect(spy.called).to.be(true);
+      deleteHelper.deleteByType('aaa', null, deleteSpy);
+      sinon.assert.called(deleteSpy);
     });
 
-    it('should call the delegated delete method method if the query is not used by any visualisations', function (done) {
-      const spy = sinon.spy();
+    it('should call the delegated delete method method if the query is not used by any visualisations', function () {
+      const deleteSpy = sinon.spy();
 
-      deleteHelper.deleteByType('query', [ '666' ], spy).then(function () {
-        expect(spy.called).to.be(true);
-        done();
+      return deleteHelper.deleteByType('query', [ '666' ], deleteSpy).then(function () {
+        sinon.assert.called(deleteSpy);
       });
     });
 
-    it('should not delete query that is used by a visualisation', function (done) {
-      const spy = sinon.spy();
-      const stub = sinon.stub(window, 'alert', function () { return false; });
+    it('should not delete query that is used by a visualisation', function () {
+      const deleteSpy = sinon.spy();
+      const alertStub = sinon.stub($window, 'alert', () => false);
 
-      deleteHelper.deleteByType('query', [ '123' ], spy).then(function () {
-        expect(spy.called).to.be(false);
-        done();
+      return deleteHelper.deleteByType('query', [ '123' ], deleteSpy).then(function () {
+        sinon.assert.called(alertStub);
+        sinon.assert.notCalled(deleteSpy);
       });
     });
 
-    it('should call the delegated delete method method if the dashboard is not in any group', function (done) {
-      const spy = sinon.spy();
+    it('should recompute dashboard groups if a group was deleted', function () {
+      const deleteSpy = sinon.spy();
 
-      deleteHelper.deleteByType('dashboard', [ 'dashboard 666' ], spy).then(function () {
-        expect(spy.called).to.be(true);
-        done();
+      return deleteHelper.deleteByType('dashboardgroup', [ 'group-1' ], deleteSpy)
+      .then(function () {
+        sinon.assert.called(deleteSpy);
+        sinon.assert.called(computeGroupsStub);
       });
     });
 
-    it('should not delete dashboard that is in a group', function (done) {
-      const spy = sinon.spy();
-      const stub = sinon.stub(window, 'alert', function () { return false; });
+    it('should call the delegated delete method method if the dashboard is not in any group', function () {
+      const deleteSpy = sinon.spy();
 
-      deleteHelper.deleteByType('dashboard', [ 'Companies' ], spy).then(function () {
-        expect(spy.called).to.be(false);
-        done();
+      return deleteHelper.deleteByType('dashboard', [ 'dashboard 666' ], deleteSpy)
+      .then(function () {
+        sinon.assert.called(deleteSpy);
+        sinon.assert.called(computeGroupsStub); // should recompute the dashboard groups
+      });
+    });
+
+    it('should not delete dashboard that is in a group', function () {
+      const deleteSpy = sinon.spy();
+      const alertStub = sinon.stub($window, 'alert', () => false);
+
+      return deleteHelper.deleteByType('dashboard', [ 'Companies' ], deleteSpy)
+      .then(function () {
+        sinon.assert.called(alertStub);
+        sinon.assert.notCalled(deleteSpy);
       });
     });
   });

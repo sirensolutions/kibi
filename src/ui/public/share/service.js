2b92c804b5548be0f92f75b4e75f53a2a2efbd12
Stéphane Campinas <stephane.campinas@gmail.com>
2017-04-11 10:42:42 +0100

    use import instead of require

--- src/ui/public/share/service.js
+++ src/ui/public/share/service.js
@@ -1,3 +1,4 @@
+import UrlShortenerProvider from './lib/url_shortener';
 import uiModules from 'ui/modules';
 import { unhashUrl, getUnhashableStatesProvider } from 'ui/state_management/state_hashing';
 
@@ -5,7 +6,7 @@ uiModules
 .get('kibana')
 .service('sharingService', function (Private, $location, config) {
 
-  const urlShortener = Private(require('./lib/url_shortener'));
+  const urlShortener = Private(UrlShortenerProvider);
   const getUnhashableStates = Private(getUnhashableStatesProvider);
 
   /**

9a816056da2da6042e648763ef857ecf04e0678c
Stéphane Campinas <stephane.campinas@gmail.com>
2017-04-21 13:12:25 +0100

    set state:storeInSessionStorage to true by default

--- src/ui/settings/defaults.js
+++ src/ui/settings/defaults.js
@@ -299,7 +299,8 @@ export default function defaultSettingsProvider(kibiEnterpriseEnabled) {
       description: '<em>[experimental]</em> Your API key from www.quandl.com'
     },
     'state:storeInSessionStorage': {
-      value: false,
+      // siren: enable this by default so that visualizations like the graph browser work
+      value: true,
       description: 'The URL can sometimes grow to be too large for some browsers to ' +
         'handle. To counter-act this we are testing if storing parts of the URL in ' +
         'sessions storage could help. Please let us know how it goes!'

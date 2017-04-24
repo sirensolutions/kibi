bb814fcea4a958d7e24efc25f2e10e7e3dc86813
szydan <danielczyk.szymon@gmail.com>
2017-04-24 17:29:26 +0100

    removed wrong pointer to kibi.dev.yml

--- src/cli/kibi/upgrade.js
+++ src/cli/kibi/upgrade.js
@@ -111,7 +111,7 @@ export default function (program) {
     .option(
       '-c, --config <path>',
       'Path to the config file, can be changed with the CONFIG_PATH environment variable as well',
-      process.env.CONFIG_PATH || fromRoot('config/kibi.dev.yml') || fromRoot('config/kibi.yml'))
+      process.env.CONFIG_PATH || fromRoot('config/kibi.yml'))
     .option(
       '--plugin-dir <path>',
       'A path to scan for plugins, this can be specified multiple ' +

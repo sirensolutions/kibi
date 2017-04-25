43eec4aee7772b07dfbd121ba81268737fdcf2af
Szymon Danielczyk <danielczyk.szymon@gmail.com>
2017-04-25 19:07:03 +0100

    missing import

--- src/ui/public/chrome/directives/index.js
+++ src/ui/public/chrome/directives/index.js
@@ -1,5 +1,5 @@
 import './global_nav';
-import './kibi_reload'; // siren: added 
+import './kibi_reload'; // siren: added
 
 import kbnChromeProv from './kbn_chrome';
 import kbnChromeNavControlsProv from './append_nav_controls';

f8181e03b7084e95781ac4239f1f328687145b44
Felixls <sergioscarnatto@gmail.com>
2017-04-27 12:45:03 +0100

    fontawesome-iconpicker instead of ui-iconpicker

---
+++ webpackShims/fontawesome-iconpicker.js
@@ -0,0 +1,4 @@
+require('jquery');
+require('node_modules/fontawesome-iconpicker/dist/js/fontawesome-iconpicker');
+require('node_modules/fontawesome-iconpicker/dist/css/fontawesome-iconpicker.css');
+

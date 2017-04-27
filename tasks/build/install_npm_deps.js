f2b0e90b6e6cdb05d3ee92653ec72c7c4e70f7d2
Colm O'Shea <colmose@gmail.com>
2017-04-27 11:02:56 +0100

    Rename getPythonPath to pythonPath

--- tasks/build/install_npm_deps.js
+++ tasks/build/install_npm_deps.js
@@ -2,11 +2,11 @@ module.exports = function (grunt) {
   const { exec } = require('child_process');
   const { resolve } = require('path');
   // siren: if on windows, use where.exe to find python path
-  const getPythonPath = (/^win/).test(process.platform) ? '"$(where.exe python2)"' : '$(which python2)';
+  const pythonPath = (/^win/).test(process.platform) ? '"$(where.exe python2)"' : '$(which python2)';
   grunt.registerTask('_build:installNpmDeps', function () {
     grunt.file.mkdir('build/kibana/node_modules');
 
-    exec(`npm install  --production --no-optional --python=${getPythonPath}`, { // siren: specify python path
+    exec(`npm install  --production --no-optional --python=${pythonPath}`, { // siren: specify python path
       cwd: grunt.config.process('<%= root %>/build/kibana')
     }, this.async());
   });

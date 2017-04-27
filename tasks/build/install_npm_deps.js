69cbf8444ee846ff69fcdf52d46495c916396990
Colm O'Shea <colmose@gmail.com>
2017-04-27 10:57:00 +0100

    Use where.exe in place of which on windows

--- tasks/build/install_npm_deps.js
+++ tasks/build/install_npm_deps.js
@@ -1,14 +1,13 @@
 module.exports = function (grunt) {
   const { exec } = require('child_process');
   const { resolve } = require('path');
-
+  // siren: if on windows, use where.exe to find python path
+  const getPythonPath = (/^win/).test(process.platform) ? '"$(where.exe python2)"' : '$(which python2)';
   grunt.registerTask('_build:installNpmDeps', function () {
     grunt.file.mkdir('build/kibana/node_modules');
 
-    exec('npm install  --production --no-optional --python=$(which python2)', {
+    exec(`npm install  --production --no-optional --python=${getPythonPath}`, { // siren: specify python path
       cwd: grunt.config.process('<%= root %>/build/kibana')
     }, this.async());
   });
 };
-
-

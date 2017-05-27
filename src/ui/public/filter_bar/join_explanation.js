20c38a1b92368106d6801fc6d65abc0473df865c
Stéphane Campinas <stephane.campinas@gmail.com>
2017-05-28 00:00:19 +0100

    fix default query condition

--- src/ui/public/filter_bar/join_explanation.js
+++ src/ui/public/filter_bar/join_explanation.js
@@ -198,7 +198,7 @@ uiModules
         }
         _.each(queryStrings, (queryString) => {
           // only if the query is different than star query
-          if (queryString.query_string.query !== '*') {
+          if (!_.has(queryString, 'query_string.query') || _.get(queryString, 'query_string.query') !== '*') {
             promises.push(explainFilter(queryString, indexId));
           }
         });

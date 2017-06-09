0bc82376be8e4a3853375a90d88de4df1c0c8189
Fabio <blackavatar@hotmail.com>
2017-06-09 15:38:13 +0200

    Filter out tinkerpop3 datasources

--- src/kibi_plugins/kibi_core/public/management/sections/kibi_queries/controllers/queries_editor.js
+++ src/kibi_plugins/kibi_core/public/management/sections/kibi_queries/controllers/queries_editor.js
@@ -259,6 +259,15 @@ function controller(kibiState, $scope, $route, kbnUrl, createNotifier, queryEngi
     }
   };
 
+  $scope.rejectTinkerpop = function (item) {
+    if (item) {
+      if (item.type === 'tinkerpop3') {
+        return true;
+      }
+    }
+    return false;
+  };
+
   //TODO understand how the validation was done in object editor
   $scope.aceLoaded = function (editor) {
     return;

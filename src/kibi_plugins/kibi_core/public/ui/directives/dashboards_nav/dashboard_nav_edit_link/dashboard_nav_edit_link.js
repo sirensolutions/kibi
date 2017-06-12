262f5a3b5062b05d8fa036b78c321e607b13749d
mehmetfurkanaydin <aydin.mehmetfurkan@gmail.com>
2017-06-12 22:57:35 +0300

    dashboard group layout fixed

--- src/kibi_plugins/kibi_core/public/ui/directives/dashboards_nav/dashboard_nav_edit_link/dashboard_nav_edit_link.js
+++ src/kibi_plugins/kibi_core/public/ui/directives/dashboards_nav/dashboard_nav_edit_link/dashboard_nav_edit_link.js
@@ -1,5 +1,6 @@
 import 'plugins/kibi_core/saved_objects/dashboard_groups/saved_dashboard_groups';
 import CacheProvider from 'ui/kibi/helpers/cache_helper';
+import jQuery from 'jquery';
 import dashboardNavEditLinkTemplate from './dashboard_nav_edit_link.html';
 import './dashboard_nav_edit_link.less';
 import uiModules from 'ui/modules';
@@ -17,7 +18,7 @@ uiModules
       group: '='
     },
     template: dashboardNavEditLinkTemplate,
-    link: function ($scope) {
+    link: function ($scope, $element) {
       const cache = Private(CacheProvider);
       const notify = createNotifier({
         location: 'Dashboard Groups Editor'
@@ -39,6 +40,75 @@ uiModules
         })
         .catch (notify.error);
       };
+
+      const groupAnchor = jQuery($element).find('.title');
+
+      $scope.isSidebarOpen = dashboardsNavState.isOpen();
+      $scope.$watch(dashboardsNavState.isOpen, isOpen => {
+        $scope.isSidebarOpen = isOpen;
+      });
+
+
+      $scope.initQtip = function () {
+        groupAnchor.qtip({
+          content: {
+            title: 'Group',
+            text: function () {
+              return $scope.group.title;
+            }
+          },
+          position: {
+            my: 'left top',
+            at: 'right center'
+          },
+          show: {
+            event: 'mouseenter',
+            solo: true
+          },
+          hide: {
+            event: 'mouseleave'
+          },
+          style: {
+            classes: 'qtip-light qtip-rounded qtip-shadow'
+          }
+        });
+      };
+
+      $scope.$watchGroup([ 'group.title', 'group.dashboards' ], () => {
+        $scope.initQtip();
+      });
+    }
+  };
+})
+.directive('dashboardNavEditTooltip', function () {
+  return {
+    scope: {
+      dashboard: '='
+    },
+    link: function ($scope, element, attrs)
+     {
+      element .qtip({
+        content: {
+          title: 'Dashboard',
+          text: function () {
+            return $scope.dashboard.title;
+          }
+        },
+        position: {
+          my: 'left top',
+          at: 'right center'
+        },
+        show: {
+          event: 'mouseenter',
+          solo: true
+        },
+        hide: {
+          event: 'mouseleave'
+        },
+        style: {
+          classes: 'qtip-light qtip-rounded qtip-shadow'
+        }
+      });
     }
   };
 });

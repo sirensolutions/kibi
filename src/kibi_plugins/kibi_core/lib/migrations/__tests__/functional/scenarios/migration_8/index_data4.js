2afa2fc9ff0fa30a9c67e25f7d951240c2fa9b14
Szymon Danielczyk <danielczyk.szymon@gmail.com>
2017-07-12 21:19:12 +0100

    migration 8

---
+++ src/kibi_plugins/kibi_core/lib/migrations/__tests__/functional/scenarios/migration_8/index_data4.js
@@ -0,0 +1,23 @@
+import requirefrom from 'requirefrom';
+
+const packageJson = requirefrom('src/utils')('package_json');
+
+/**
+ * Defines the following objects:
+ *
+ * - a configuration with an empty kibi:relations
+ */
+module.exports = [
+  {
+    index: {
+      _index: '.kibi4',
+      _type: 'config',
+      _id: 'kibi'
+    }
+  },
+  {
+    buildNum: packageJson.build.number,
+    'dateFormat:tz': 'UTC',
+    'kibi:relations': ''
+  }
+];

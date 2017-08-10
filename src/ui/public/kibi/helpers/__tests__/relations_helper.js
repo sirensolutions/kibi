5b54be387192e8caab2977f9830a26d4fc75b007
mehmetfurkanaydin <aydin.mehmetfurkan@gmail.com>
2017-08-10 11:38:00 +0300

    reverted relations_helper tests

--- src/ui/public/kibi/helpers/__tests__/relations_helper.js
+++ src/ui/public/kibi/helpers/__tests__/relations_helper.js
@@ -437,7 +437,179 @@ describe('Kibi Components', function () {
       });
     });
 
+    describe('addAdvancedJoinSettingsToRelation', function () {
+      beforeEach(() => init({
+        kibiEnterpriseEnabled: true
+      }));
+
+      it('should not fail if the relation is missing', function () {
+        const relations = {
+          relationsIndices: [
+            {
+              indices: [
+                {
+                  indexPatternId: 'investor',
+                  path: 'id'
+                },
+                {
+                  indexPatternId: 'investment',
+                  path: 'investorid'
+                }
+              ],
+              label: 'by',
+              id: 'investment/investorid/investor/id'
+            }
+          ]
+        };
+        const missingRelation = [
+          {
+            indices: [ 'company' ],
+            path: 'id'
+          },
+          {
+            indices: [ 'article' ],
+            path: 'companies'
+          }
+        ];
+
+        $rootScope.$emit('change:config.kibi:relations', relations);
+        $rootScope.$digest();
+
+        expect(relationsHelper.addAdvancedJoinSettingsToRelation).withArgs(missingRelation).to.be.ok();
+      });
+
+      it('should get advanced relation for the given relation', function () {
+        const relations = {
+          relationsIndices: [
+            {
+              indices: [
+                {
+                  indexPatternId: 'investor',
+                  path: 'id',
+                  termsEncoding: 'enc1',
+                  orderBy: 'asc',
+                  maxTermsPerShard: 1
+                },
+                {
+                  indexPatternId: 'investment',
+                  path: 'investorid',
+                  termsEncoding: 'enc2',
+                  orderBy: 'desc',
+                  maxTermsPerShard: 2
+                }
+              ],
+              label: 'by',
+              id: 'investment//investorid/investor//id'
+            }
+          ]
+        };
+        const relation1 = [
+          {
+            indices: [ 'investment' ],
+            path: 'investorid'
+          },
+          {
+            indices: [ 'investor' ],
+            path: 'id'
+          }
+        ];
+
+        $rootScope.$emit('change:config.kibi:relations', relations);
+        $rootScope.$digest();
+
+        relationsHelper.addAdvancedJoinSettingsToRelation(relation1);
+        expect(relation1[0].termsEncoding).to.be('enc1');
+        expect(relation1[0].orderBy).to.be('asc');
+        expect(relation1[0].maxTermsPerShard).to.be(1);
+        expect(relation1[1].termsEncoding).to.be('enc2');
+        expect(relation1[1].orderBy).to.be('desc');
+        expect(relation1[1].maxTermsPerShard).to.be(2);
+
+        const relation2 = [
+          {
+            indices: [ 'investor' ],
+            path: 'id'
+          },
+          {
+            indices: [ 'investment' ],
+            path: 'investorid'
+          }
+        ];
+        relationsHelper.addAdvancedJoinSettingsToRelation(relation2);
+        expect(relation2[0].termsEncoding).to.be('enc2');
+        expect(relation2[0].orderBy).to.be('desc');
+        expect(relation2[0].maxTermsPerShard).to.be(2);
+        expect(relation2[1].termsEncoding).to.be('enc1');
+        expect(relation2[1].orderBy).to.be('asc');
+        expect(relation2[1].maxTermsPerShard).to.be(1);
+      });
+
+      it('should get advanced relation with the specified patterns', function () {
+        const relations = {
+          relationsIndices: [
+            {
+              indices: [
+                {
+                  indexPatternId: 'weather-*',
+                  path: 'forecast',
+                  termsEncoding: 'enc1',
+                  orderBy: 'asc',
+                  maxTermsPerShard: 1
+                },
+                {
+                  indexPatternId: 'forecast',
+                  path: 'forecast',
+                  termsEncoding: 'enc2',
+                  orderBy: 'desc',
+                  maxTermsPerShard: 2
+                }
+              ],
+              label: 'label',
+              id: 'forecast//forecast/weather-*//forecast'
+            }
+          ]
+        };
+        const relation1 = [
+          {
+            indices: [ 'forecast' ],
+            path: 'forecast'
+          },
+          {
+            indices: [ 'weather-2015-01', 'weather-2015-02' ],
+            path: 'forecast'
+          }
+        ];
+
+        $rootScope.$emit('change:config.kibi:relations', relations);
+        $rootScope.$digest();
+
+        relationsHelper.addAdvancedJoinSettingsToRelation(relation1, 'forecast', 'weather-*');
+        expect(relation1[0].termsEncoding).to.be('enc1');
+        expect(relation1[0].orderBy).to.be('asc');
+        expect(relation1[0].maxTermsPerShard).to.be(1);
+        expect(relation1[1].termsEncoding).to.be('enc2');
+        expect(relation1[1].orderBy).to.be('desc');
+        expect(relation1[1].maxTermsPerShard).to.be(2);
+
+        const relation2 = [
+          {
+            indices: [ 'weather-2015-01', 'weather-2015-02' ],
+            path: 'forecast'
+          },
+          {
+            indices: [ 'forecast' ],
+            path: 'forecast'
+          }
+        ];
+        relationsHelper.addAdvancedJoinSettingsToRelation(relation2, 'weather-*', 'forecast');
+        expect(relation2[0].termsEncoding).to.be('enc2');
+        expect(relation2[0].orderBy).to.be('desc');
+        expect(relation2[0].maxTermsPerShard).to.be(2);
+        expect(relation2[1].termsEncoding).to.be('enc1');
+        expect(relation2[1].orderBy).to.be('asc');
+        expect(relation2[1].maxTermsPerShard).to.be(1);
+      });
+    });
   });
 });
-
 

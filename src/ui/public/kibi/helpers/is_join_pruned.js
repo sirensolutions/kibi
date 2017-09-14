a1ed094ffba9ef6eef195104b8ae2766c87ac958
Szymon Danielczyk <danielczyk.szymon@gmail.com>
2017-09-14 12:38:26 +0100

    Update the is_pruned logic

--- src/ui/public/kibi/helpers/is_join_pruned.js
+++ src/ui/public/kibi/helpers/is_join_pruned.js
@@ -1,11 +1,3 @@
-module.exports = function (hit) {
-  if (hit.coordinate_search) {
-    const actions = hit.coordinate_search.actions;
-    for (let j = 0; j < actions.length; j++) {
-      if (actions[j].is_pruned) {
-        return true;
-      }
-    }
-  }
-  return false;
+module.exports = function (response) {
+  return response.planner && response.planner.is_pruned === true;
 };

4ed8553c502d0006061a0be452e429a0e4035f5b
Fabio <fabio.tacchelli@gmail.com>
2017-10-20 17:09:19 +0200

    Add ontology client and initial tests

---
+++ src/ui/public/kibi/components/ontology_client/__tests__/ontology_client.js
@@ -0,0 +1,117 @@
+import ngMock from 'ng_mock';
+
+let $httpBackend;
+let ontologyClient;
+
+describe('Kibi Components', function () {
+  describe('Ontology client', function () {
+
+    beforeEach(function () {
+      ngMock.module('kibana');
+
+      ngMock.module('app/dashboard', function ($provide) {
+        $provide.service('savedDashboards', function () {
+          return {
+            find: function () {
+              return Promise.resolve({ hits: []});
+            }
+          };
+        });
+      });
+
+      ngMock.inject(function ($injector) {
+        $httpBackend = $injector.get('$httpBackend');
+        ontologyClient = $injector.get('ontologyClient');
+      });
+    });
+
+    afterEach(function () {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('gets the relations from the ontology model', function () {
+      $httpBackend.expectPOST(/\/schema/).respond();
+      ontologyClient.getRelations();
+      $httpBackend.flush();
+    });
+
+    it('gets ranges for the given entity', function () {
+      $httpBackend.expectPOST(/\/schema/).respond();
+      ontologyClient.getRangesForEntityId('id');
+      $httpBackend.flush();
+    });
+
+    it('gets the relations for the given domain', function () {
+      $httpBackend.expectPOST(/\/schema/).respond();
+      ontologyClient.getRelationsByDomain('id');
+      $httpBackend.flush();
+    });
+
+    it('gets the unique relation labels', function () {
+      $httpBackend.expectPOST(/\/schema/).respond();
+      ontologyClient.getUniqueRelationLabels();
+      $httpBackend.flush();
+    });
+
+    it('gets the unique field descriptions', function () {
+      $httpBackend.expectPOST(/\/schema/).respond();
+      ontologyClient.getUniqueFieldDescriptions();
+      $httpBackend.flush();
+    });
+
+    it('gets all the entities', function () {
+      $httpBackend.expectPOST(/\/schema/).respond();
+      ontologyClient.getEntities();
+      $httpBackend.flush();
+    });
+
+    it('gets an entity by id', function () {
+      $httpBackend.expectPOST(/\/schema/).respond({ data: { id: 'something'}});
+      ontologyClient.getEntityById('id');
+      $httpBackend.flush();
+    });
+
+    it('gets the dashboard given an entity id', function () {
+      $httpBackend.expectPOST(/\/schema/).respond();
+      ontologyClient.getDashboardsByEntity({});
+      $httpBackend.flush();
+    });
+
+    it('inserts an array of relations', function () {
+      $httpBackend.expectPOST(/\/schema/).respond();
+      ontologyClient.insertRelations([{}]);
+      $httpBackend.flush();
+    });
+
+    it('inserts an entity', function () {
+      $httpBackend.expectPOST(/\/schema/).respond();
+      ontologyClient.insertEntity({ id: 'id' });
+      $httpBackend.flush();
+    });
+
+    it('executes a SPARQL update query', function () {
+      $httpBackend.expectPOST(/\/schema/).respond();
+      ontologyClient.updateWithQuery({});
+      $httpBackend.flush();
+    });
+
+    it('updates an existing entity', function () {
+      $httpBackend.expectPOST(/\/schema/).respond();
+      ontologyClient.updateEntity({});
+      $httpBackend.flush();
+    });
+
+    it('deletes an entity', function () {
+      $httpBackend.expectPOST(/\/schema/).respond();
+      ontologyClient.deleteEntity('id');
+      $httpBackend.flush();
+    });
+
+    it('deletes all relations that have the given entity as domain or range', function () {
+      $httpBackend.expectPOST(/\/schema/).respond();
+      ontologyClient.deleteByDomainOrRange('id');
+      $httpBackend.flush();
+    });
+  });
+});
---
+++ src/ui/public/kibi/components/ontology_client/ontology_client.js
@@ -0,0 +1,336 @@
+import _ from 'lodash';
+import { uiModules } from 'ui/modules';
+
+uiModules
+.get('kibana/ontology_client')
+.service('ontologyClient', function (Private, queryEngineClient, createNotifier, savedDashboards, savedSearches) {
+  const notify = createNotifier();
+
+  const defaultKibiNs = 'http://siren.io/model#';
+
+  function OntologyClient() {
+    this._cachedEntityRanges = {};
+    this._cachedEntitiesMap = {};
+  }
+
+  OntologyClient.prototype._decodeUrl = function (str) {
+    const encodedUrl = str.replace(/%21/g, '!')
+    .replace(/%27/g, '\'')
+    .replace(/'%28'/g, '(')
+    .replace(/%29/g, ')')
+    .replace(/%2A/g, '*')
+    .replace(/\+/g, '%20');
+    return decodeURIComponent(encodedUrl);
+  };
+
+  OntologyClient.prototype._removeNs = function (str) {
+    const value = str.replace(defaultKibiNs, '');
+    return value;
+  }
+
+  OntologyClient.prototype._removeNsDecode = function (str) {
+    if (!str) {
+      return str;
+    }
+    const value = this._removeNs(str);
+    return this._decodeUrl(value);
+  };
+
+  OntologyClient.prototype._encodeUrl = function (str) {
+    return encodeURIComponent(str)
+    .replace(/!/g, '%21')
+    .replace(/'/g, '%27')
+    .replace(/\(/g, '%28')
+    .replace(/\)/g, '%29')
+    .replace(/\*/g, '%2A')
+    .replace(/%20/g, '+');
+  };
+
+  /*
+   * Returns the list of available relations.
+   */
+  OntologyClient.prototype.getRelations = function () {
+    if (this._cachedRelationsList) {
+      const clonedRelations = _.cloneDeep(this._cachedRelationsList);
+      return Promise.resolve(clonedRelations);
+    } else {
+      return queryEngineClient.schema({
+        path: '/schema/relations',
+        method: 'GET'
+      })
+      .then((res) => {
+        if (res.status !== 200) {
+          Promise.reject(new Error('Failed to retrieve relations from the schema. An error has occurred.'));
+        } else if (res.data) {
+          const relations = _.reduce(res.data, (relationsArray, rel) => {
+            rel.id = this._removeNsDecode(rel.id);
+            rel.domain.id = this._removeNsDecode(rel.domain.id);
+            rel.range.id = this._removeNsDecode(rel.range.id);
+            if (rel.inverseOf) {
+              rel.inverseOf = this._removeNsDecode(rel.inverseOf);
+            }
+
+            relationsArray.push(rel);
+            return relationsArray;
+          }, []);
+
+          this._cachedRelationsList = relations;
+          return relations;
+        }
+        Promise.reject(new Error('Failed to retrieve relations from the schema. No data retrieved.'));
+      });
+    }
+  };
+
+  /*
+   * Returns a list of ranges for a given entity id. The objects returned in the list have the type too.
+   * Eg: [{ id: 'Article', type: 'INDEX_PATTERN' }]
+   */
+  OntologyClient.prototype.getRangesForEntityId = function (entityId) {
+    if (this._cachedEntityRanges[entityId]) {
+      const clonedRanges = _.cloneDeep(this._cachedEntityRanges[entityId]);
+      return Promise.resolve(clonedRanges);
+    } else {
+      return this.getRelations()
+      .then((relations) => {
+        const ranges = _.reduce(relations, (total, relation) => {
+          if (relation.domain.id === entityId) {
+            total.push({
+              id: relation.range.id,
+              type: relation.range.type
+            });
+          }
+          return total;
+        }, []);
+        this._cachedEntityRanges[entityId] = ranges;
+        return ranges;
+      });
+    }
+  };
+
+  /*
+   * Returns a list of relations having the given entity id as domain.
+   */
+  OntologyClient.prototype.getRelationsByDomain = function (domainId) {
+    return this.getRelations()
+    .then((relations) => {
+      const filteredRelations = _.filter(relations, (rel) => {
+        return rel.domain.indexPattern === domainId;
+      });
+      return filteredRelations;
+    });
+  };
+
+  /*
+   * Returns the list of relation labels (unique values)
+   */
+  OntologyClient.prototype.getUniqueRelationLabels = function () {
+    return this.getRelations()
+    .then((relations) => {
+      const relationLabelsSet = _.reduce(relations, (total, rel) => {
+        total.add(rel.directLabel);
+        total.add(rel.inverseLabel);
+        return total;
+      }, new Set());
+      return Array.from(relationLabelsSet);
+    });
+  };
+
+  /*
+   *
+   */
+  OntologyClient.prototype.getUniqueFieldDescriptions = function () {
+    return this.getRelations()
+    .then((relations) => {
+      const fieldDescriptions = _.reduce(relations, (total, rel) => {
+        if (rel.domain.fieldDescr) {
+          total.push(rel.domain.fieldDescr);
+        } else if (rel.range.fieldDescr) {
+          total.push(rel.range.fieldDescr);
+        }
+        return total;
+      }, new Set());
+      return Array.from(fieldDescriptions);
+    });
+  };
+
+  /*
+   * Returns the list of available entities.
+   */
+  OntologyClient.prototype.getEntities = function () {
+    if (this._cachedEntitiesList) {
+      const clonedEntities = _.cloneDeep(this._cachedEntitiesList);
+      return Promise.resolve(clonedEntities);
+    } else {
+      return queryEngineClient.schema({
+        path: '/schema/entities',
+        method: 'GET'
+      })
+      .then((res) => {
+        if (res.status !== 200) {
+          Promise.reject(new Error('Failed to retrieve entities from the schema. An error has occurred.'));
+        } else if (res.data) {
+          const entities = _.reduce(res.data, (total, entity) => {
+            entity.id = this._removeNsDecode(entity.id),
+            entity.longDescr = this._removeNsDecode(entity.longDescr);
+
+            total.push(entity);
+            return total;
+          }, []);
+          this._cachedEntitiesList = entities;
+          console.log(entities);
+          return entities;
+        }
+        Promise.reject(new Error('Failed to retrieve entities from the schema. No data retrieved.'));
+      });
+    }
+  };
+
+  /*
+   * Returns an entity by id.
+   */
+  OntologyClient.prototype.getEntityById = function (entityId) {
+    if (this._cachedEntitiesMap[entityId]) {
+      const clonedEntity = _.cloneDeep(this._cachedEntitiesMap[entityId]);
+      return Promise.resolve(clonedEntity);
+    } else {
+      return this.getEntities()
+      .then((entities) => {
+        const entity = _.find(entities, (entity) => { return entity.id === entityId; });
+        if (entity) {
+          this._cachedEntitiesMap[entity.id] = entity;
+        }
+        return entity;
+      });
+    }
+  };
+
+  /*
+   * Returns a list of dashboards that can be the target of a join for the given entity id.
+   */
+  OntologyClient.prototype.getDashboardsByEntity = function (entity) {
+    return this.getRangesForEntityId(entity.indexPattern)
+    .then((ranges) => {
+      const indexPatterns = new Set(_.map(ranges, 'indexPattern'));
+      return savedDashboards.find().then(function (dashboards) {
+        const promises = [];
+        _.each(dashboards.hits, (dashboard) => {
+          promises.push(savedSearches.get(dashboard.savedSearchId).then((savedSearch) => {
+            if (savedSearch.kibanaSavedObjectMeta && savedSearch.kibanaSavedObjectMeta.searchSourceJSON) {
+              const index = JSON.parse(savedSearch.kibanaSavedObjectMeta.searchSourceJSON).index;
+              if (indexPatterns.has(index)) {
+                return dashboard;
+              }
+            }
+          }));
+        });
+        return Promise.all(promises).then((dashboards) => {
+          return _.filter(dashboards, (dashboard) => {
+            return !!dashboard;
+          });
+        });
+      });
+    });
+  };
+
+  /*
+   * Inserts a list of relations into the relational model.
+   */
+  OntologyClient.prototype.insertRelations = function (relations) {
+    return queryEngineClient.schema({
+      path: '/schema/relations',
+      method: 'POST',
+      data: relations
+    })
+    .then(() => {
+      this.clearCache();
+    });
+  };
+
+  /*
+   * Inserts an entity into the relational model.
+   */
+  OntologyClient.prototype.insertEntity = function (entity) {
+    if (!entity.id) {
+      return Promise.reject('Missing entity id');
+    } else {
+      const encodedId = this._encodeUrl(entity.id);
+      return queryEngineClient.schema({
+        path: '/schema/entity/' + encodedId,
+        method: 'POST',
+        data: entity
+      })
+      .then(() => {
+        this.clearCache();
+      });
+    }
+  }
+
+  /*
+   * Executes the passed SPARQLE UPDATE query.
+   */
+  OntologyClient.prototype.updateWithQuery = function (query) {
+    if (query) {
+      return queryEngineClient.schema({
+        path: '/schema/update',
+        method: 'POST',
+        data: { query: query }
+      })
+      .then(() => {
+        this.clearCache();
+      });
+    } else {
+      return Promise.reject('You are trying to issue a SPARQL update with an empty query');
+    }
+  };
+
+  /**
+   * Updates the properties of an entity.
+   */
+  OntologyClient.prototype.updateEntity = function (entity) {
+    const encodedId = this._encodeUrl(entity.id);
+    return queryEngineClient.schema({
+      path: '/schema/entity/' + encodedId,
+      method: 'PUT',
+      data: entity
+    })
+    .then(() => {
+      this.clearCache();
+    });
+  };
+
+  /*
+   * Delete an entity by id.
+   */
+  OntologyClient.prototype.deleteEntity = function (entityId) {
+    return queryEngineClient.schema({
+      path: '/schema/entity/' + this._encodeUrl(entityId),
+      method: 'DELETE'
+    })
+    .then(() => {
+      this.clearCache();
+    });
+  };
+
+  /*
+   * Delete all relations having the passed entity as domain or range.
+   */
+  OntologyClient.prototype.deleteByDomainOrRange = function (entityId) {
+    return queryEngineClient.schema({
+      path: '/schema/relationByDomainOrRange/' + this._encodeUrl(entityId),
+      method: 'DELETE'
+    })
+    .then(() => {
+      this.clearCache();
+    });
+  };
+
+  OntologyClient.prototype.clearCache = function () {
+    this._cachedEntitiesList = null;
+    this._cachedEntitiesMap = {};
+    this._cachedRelationsList = null;
+    this._cachedEntityRanges = {};
+  };
+
+  return new OntologyClient();
+});

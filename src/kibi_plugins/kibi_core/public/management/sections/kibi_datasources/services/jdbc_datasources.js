a50ef68aeaefafd91a602d87f3f965786f8d9c66
Szymon Danielczyk <danielczyk.szymon@gmail.com>
2017-10-24 10:39:21 +0100

    The virtual indices tab

--- src/kibi_plugins/kibi_core/public/management/sections/kibi_datasources/services/jdbc_datasources.js
+++ src/kibi_plugins/kibi_core/public/management/sections/kibi_datasources/services/jdbc_datasources.js
@@ -2,56 +2,63 @@ import chrome from 'ui/chrome';
 import { uiModules } from 'ui/modules';
 uiModules
 .get('kibi_datasources/services/jdbc_datasources')
-.service('jdbcDatasources', function ($http) {
+.service('jdbcDatasources', function ($http, Promise) {
 
   class JdbcDatasources {
     constructor() {
-      this.baseUrl = chrome.getBasePath() + '/elasticsearch/_vanguard/connector/datasource/';
+      this.baseUrl = chrome.getBasePath() + '/elasticsearch/_vanguard/connector';
     }
+
     get(id) {
-      return $http.get(this.baseUrl + id).then(resp => resp.data);
+      return $http.get(this.baseUrl + '/datasource/' + id).then(resp => resp.data);
     }
 
     list() {
       // TODO: remove when Fabio will give me new jar
-      return Promise.resolve([
-        {
-          _id: 'mysql',
-          _source: {
-            jdbc: {
-              username: 'root',
-              password: 'password',
-              driver: 'com.mysql.jdbc.Driver',
-              url: 'jdbc:mysql://localhost:3306/employees'
-            }
-          }
-        },
-        {
-          _id: 'ccc',
-          _source: {
-            jdbc: {
-              username: 'root',
-              password: 'password',
-              driver: 'com.mysql.jdbc.Driver',
-              url: 'jdbc:mysql://localhost:3306/employees'
-            }
-          }
-        }
-      ]);
-      //return $http.get(chrome.getBasePath() + '/elasticsearch/_vanguard/connector/datasource/');
+      return $http.get(chrome.getBasePath() + '/elasticsearch/.vanguard-datasources/_search?size=100').then(res => {
+        return res.data.hits.hits;
+      });
     }
 
     save(datasource) {
-      return $http.put(this.baseUrl + datasource._id, datasource._source);
+      return $http.put(this.baseUrl + '/datasource/' + datasource._id, datasource._source);
     }
 
     delete(id) {
-      return $http.delete(this.baseUrl + id);
+      return $http.delete(this.baseUrl + '/datasource/' + id);
     }
 
     validate(datasource) {
-      return $http.post(this.baseUrl + datasource._id + '/_validate', datasource._source).then(res => res.data);
+      return $http.post(this.baseUrl + '/datasource/' + datasource._id + '/_validate', datasource._source).then(res => res.data);
     }
+
+    getVirtualIndex(id) {
+      return $http.get(this.baseUrl + '/index/' + id).then(res => res.data);
+    }
+
+    deleteVirtualIndex(id) {
+      return $http.delete(this.baseUrl + '/index/' + id);
+    }
+
+    createVirtualIndex(index) {
+      return $http.put(this.baseUrl + '/index/' + index._id, index._source).then(res => {
+        if (res.status === 201) {
+          return res.data;
+        } else if (res.status === 200) {
+          return Promise.reject(new Error('Indexpattern alrteady exist'));
+        } else {
+          return Promise.reject(new Error('Could not create an index pattern ' + JSON.stringify(res)));
+        }
+      });
+    }
+
+    listVirtualIndices() {
+      // TODO remove this hardcoded stuff
+      return $http.get(chrome.getBasePath() + '/elasticsearch/.vanguard-indices/_search?size=100').then(res => {
+        return res.data.hits.hits;
+      });
+    }
+
   }
 
   return new JdbcDatasources();
---
+++ src/kibi_plugins/kibi_core/public/management/sections/kibi_virtual_indices/controllers/virtual_indices_controller.js
@@ -0,0 +1,131 @@
+import { find } from 'lodash';
+import uiRoutes from 'ui/routes';
+import { uiModules } from 'ui/modules';
+import template from 'plugins/kibi_core/management/sections/kibi_virtual_indices/index.html';
+import { CONFIRM_BUTTON, CANCEL_BUTTON } from 'ui_framework/components/modal/confirm_modal';
+
+uiRoutes
+.when('/management/siren/virtualindices', {
+  template,
+  reloadOnSearch: false,
+});
+
+function controller($scope, jdbcDatasources, createNotifier, es, confirmModal) {
+
+  const notify = createNotifier({
+    location: 'Virtual Index Pattern Editor'
+  });
+
+  const fetchVirtualIndexes = function () {
+    jdbcDatasources.listVirtualIndices().then(virtualIndexPatterns => {
+      $scope.virtualIndexPatterns = virtualIndexPatterns;
+    });
+  };
+
+  fetchVirtualIndexes();
+
+  // kibi: code to handle creation of jdbc datasource based index patterns
+  $scope.jdbcFormValues = {
+    datasource: null,
+    resource: null,
+    key: null,
+    name: null
+  };
+
+  $scope.jdbcDatasources = [];
+  jdbcDatasources.list().then(datasources => {
+    $scope.jdbcDatasources = datasources;
+  });
+
+  $scope.registerJdbcIndexPattern = () => {
+    const index = {
+      _id: $scope.jdbcFormValues.name,
+      _source: {
+        datasource: $scope.jdbcFormValues.datasource,
+        resource: $scope.jdbcFormValues.resource,
+        key: $scope.jdbcFormValues.key
+      }
+    };
+
+    jdbcDatasources.createVirtualIndex(index).then(res => {
+      if (res.created === true) {
+        notify.info('Virtual Index Pattern created');
+        fetchVirtualIndexes();
+      }
+    }).catch(err => {
+      notify.error(err);
+    });
+  };
+  // kibiL: end
+
+  $scope.delete = function (id) {
+    // first check if there is an underlying physical pattern
+    es.indices.get({ index: id })
+    .then(res => {
+      if (res[id]) {
+        // found a physical index lets ask user to delete it as well
+        const confirmModalOptions = {
+          confirmButtonText: 'Delete the virtual index',
+          onConfirm: () => {
+            jdbcDatasources.deleteVirtualIndex(id).then(() => {
+              notify.info(`Virtual index pattern ${id} successfully deleted`);
+              fetchVirtualIndexes();
+            }).catch(err => {
+              notify.error(err);
+            });
+          }
+        };
+        confirmModal(
+          `You are about to delete the virtual index pattern [${id}].
+           There is also an underlying physical index with the same name [${id}].`,
+          confirmModalOptions
+        );
+      }
+    })
+    .catch(err => {
+      if (err.status === 404 && err.displayName === 'NotFound') {
+        // just delete the virtual one
+        jdbcDatasources.deleteVirtualIndex(id).then(() => {
+          notify.info('Virtual index pattern deleted');
+        })
+        .catch(err => {
+          notify.error(err);
+        });
+      } else {
+        notify.error(err);
+      }
+    });
+  };
+
+}
+
+uiModules
+.get('apps/management', ['kibana'])
+.controller('VirtualIndicesController', controller)
+.directive('validateJdbcIndexName', function () {
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function ($scope, elem, attr, ngModel) {
+      const illegalCharacters = ['\\', '/', '?', '"', '<', '>', '|', ' ', ','];
+      const isValid = function (input) {
+        if (input == null || input === '' || input === '.' || input === '..') return false;
+
+        const match = find(illegalCharacters, function (character) {
+          return input.indexOf(character) >= 0;
+        });
+        if (match) {
+          return false;
+        }
+        if (input.toLowerCase() !== input) {
+          return false;
+        }
+        return true;
+      };
+
+      ngModel.$validators.indexNameInput = function (modelValue, viewValue) {
+        return isValid(viewValue);
+      };
+    }
+  };
+});

c2357959e40cb2c1317ce29fc90b77cb5df61ec9
Szymon Danielczyk <danielczyk.szymon@gmail.com>
2017-11-01 14:09:53 +0000

    reverted min_term_freq to 1 as 2 produces no results most of the time

--- src/kibi_plugins/kibi_data_table_vis/public/kibi_data_table_vis_params.js
+++ src/kibi_plugins/kibi_data_table_vis/public/kibi_data_table_vis_params.js
@@ -113,7 +113,7 @@ uiModules
           // set the default options
           $scope.vis.params.moreLikeThisTemplate = JSON.stringify({
             max_query_terms : 25,
-            min_term_freq : 2,
+            min_term_freq : 1,
             max_doc_freq: 0,
             min_word_length: 0,
             max_word_length: 0,

f0827153618f8bf62f21463d9e105a8de99b4138
Szymon Danielczyk <danielczyk.szymon@gmail.com>
2017-11-03 16:56:21 +0000

    Corrected logic and typos

--- src/kibi_plugins/siren_federate/lib/__tests__/is_vanguard_version_greater_or_equal.js
+++ src/kibi_plugins/siren_federate/lib/__tests__/is_vanguard_version_greater_or_equal.js
@@ -3,16 +3,20 @@ import { isVanguardVersionGreaterOrEqual } from '../is_vanguard_version_greater_
 
 describe('is_vanguard_version_greater_or_equal', function () {
 
-  it('shoud say no', function () {
+  it('should return false', function () {
     expect(isVanguardVersionGreaterOrEqual('5.4.3', '5.4.3-1')).to.equal(false);
     expect(isVanguardVersionGreaterOrEqual('5.5.2', '5.5.2-1')).to.equal(false);
+
+    expect(isVanguardVersionGreaterOrEqual('7.8.9-1', '7.8.9-2')).to.equal(false);
   });
 
-  it('shoud say yes', function () {
+  it('should return true', function () {
     expect(isVanguardVersionGreaterOrEqual('5.4.3-1', '5.4.3-1')).to.equal(true);
     expect(isVanguardVersionGreaterOrEqual('5.5.2-1', '5.5.2-1')).to.equal(true);
 
     expect(isVanguardVersionGreaterOrEqual('5.4.3-2', '5.4.3-1')).to.equal(true);
     expect(isVanguardVersionGreaterOrEqual('5.5.2-2', '5.5.2-1')).to.equal(true);
+
+    expect(isVanguardVersionGreaterOrEqual('7.8.9-12', '7.8.9-11')).to.equal(false);
   });
 });

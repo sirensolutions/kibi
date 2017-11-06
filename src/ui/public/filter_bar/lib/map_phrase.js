517e68dc7e687a317ea635f7df54233c12318a23
Szymon Danielczyk <danielczyk.szymon@gmail.com>
2017-11-06 11:47:18 +0000

    Added the check for the map_phrase

--- src/ui/public/filter_bar/lib/map_phrase.js
+++ src/ui/public/filter_bar/lib/map_phrase.js
@@ -13,6 +13,11 @@ export function FilterBarLibMapPhraseProvider(Promise, courier) {
       const type = 'phrase';
       const key = isScriptedPhraseFilter ? filter.meta.field : Object.keys(filter.query.match)[0];
       const field = indexPattern.fields.byName[key];
+      // kibi: handle case where the field is no longer present in the index-pattern
+      if (!field) {
+        return Promise.reject(filter);
+      }
+      // kibi: end
       const query = isScriptedPhraseFilter ? filter.script.script.params.value : filter.query.match[key].query;
       const value = field.format.convert(query);
       return { type, key, value };

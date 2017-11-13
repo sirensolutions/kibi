6ba880f29666a7059938264df60ef25b886ad97f
mehmetfurkanaydin <aydin.mehmetfurkan@gmail.com>
2017-11-13 11:25:51 +0000

    code improved extra checking is added

--- src/core_plugins/kibana/public/dev_tools/directives/translate_join_query.js
+++ src/core_plugins/kibana/public/dev_tools/directives/translate_join_query.js
@@ -18,7 +18,7 @@ app.directive('translateJoinQuery', function ($http) {
 
       $scope.translate = function () {
         $http.post(chrome.getBasePath() + '/translateToES',  {
-          query: JSON.parse($scope.rawQuery)
+          query: $scope.rawQuery
         })
         .then(result => {
           $scope.editor.setValue(JSON.stringify(result.data.translatedQuery, null, 2));

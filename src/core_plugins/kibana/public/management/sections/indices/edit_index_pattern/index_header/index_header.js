5343c3ca3728b671b71b0df7a42957f712e83961
Fabio <fabio.tacchelli@gmail.com>
2017-12-21 12:24:31 +0100

    Add relational graph

--- src/core_plugins/kibana/public/management/sections/indices/edit_index_pattern/index_header/index_header.js
+++ src/core_plugins/kibana/public/management/sections/indices/edit_index_pattern/index_header/index_header.js
@@ -13,12 +13,15 @@ uiModules
       delete: '&',
       // kibi: indexPattern property replaced by entity, added save function
       entity: '=',
-      save: '&'
+      save: '&',
+      toggleGraph: '&'
     },
     link: function ($scope, $el, attrs) {
       $scope.delete = attrs.delete ? $scope.delete : null;
       // kibi: added save to enable saving of changes
       $scope.save = attrs.save ? $scope.save : null;
+      // kibi: added toggleGraph to enable/disable the relational graph
+      $scope.toggleGraph = attrs.toggleGraph ? $scope.toggleGraph : null;
       $scope.setDefault = attrs.setDefault ? $scope.setDefault : null;
       $scope.refreshFields = attrs.refreshFields ? $scope.refreshFields : null;
       config.bindToScope($scope, 'defaultIndex');
--- src/kibi_plugins/investigate_core/public/management/sections/kibi_entities/controllers/entities.js
+++ src/kibi_plugins/investigate_core/public/management/sections/kibi_entities/controllers/entities.js
@@ -3,14 +3,17 @@ import uiRoutes from 'ui/routes';
 import { uiModules } from 'ui/modules';
 import _ from 'lodash';
 import chrome from 'ui/chrome';
-import template from 'plugins/investigate_core/management/sections/kibi_entities/index.html';
-import 'plugins/investigate_core/ui/directives/saved_search_nav/saved_search_nav';
+import template from 'plugins/kibi_core/management/sections/kibi_entities/index.html';
+import linesPacked from 'plugins/kibi_graph_browser_vis/webpackShims/lines-packed';
+import GraphHelperClass from 'plugins/kibi_graph_browser_vis/kibi_graph_browser_helper';
+import 'plugins/kibi_core/ui/directives/saved_search_nav/saved_search_nav';
 import 'plugins/kibana/management/sections/indices/edit_index_pattern/edit_index_pattern';
 import 'plugins/investigate_core/management/sections/kibi_entities/styles/entities.less';
 import 'plugins/investigate_core/management/sections/indices/index_options/index_options';
 import './entity_relations';
 import './create_index_pattern';
 import './create_eid';
+import 'angular-animate';
 import 'angular-ui-tree';
 
 uiRoutes
@@ -65,8 +68,8 @@ uiRoutes
   }
 });
 
-uiModules.get('apps/management', ['kibana', 'ui.tree'])
-.controller('entities', function ($scope, $route, kbnUrl, createNotifier, indexPatterns, ontologyClient) {
+uiModules.get('apps/management', ['kibana', 'ui.tree', 'ngAnimate', /*'ngKeylines'*/])
+.controller('entities', function ($scope, $route, $timeout, Private, kbnUrl, createNotifier, indexPatterns, ontologyClient) {
   $scope.state = { section: 'entity_panel' };
 
   const notify = createNotifier({
@@ -93,4 +96,177 @@ uiModules.get('apps/management', ['kibana', 'ui.tree'])
       }
     }
   });
+
+  //
+  // Relational graph stuffs
+  //
+  const GraphHelper = Private(GraphHelperClass);
+  const graphHelper = new GraphHelper({});
+  const kl = linesPacked.lines;
+  let relationalGraph;
+  let graphInitialized = false;
+  $scope.isRelationalGraphVisible = false;
+  $scope.klOptions = {
+    iconFontFamily: 'FontAwesome',
+    backColour: 'white',
+    overview: false,
+    controlColour: 'grey',
+    selectionColour: '#44D',
+    hover: 100,
+    selfLinks: true,
+    handMode: true
+  };
+
+  $scope.klBasePath = chrome.getBasePath() + '/plugins/kibi_graph_browser_vis/webpackShims/';
+  $scope.toggleRelationalGraph = function () {
+    $scope.isRelationalGraphVisible = !$scope.isRelationalGraphVisible;
+  };
+
+  const updateRelationalGraph = function () {
+    // wait for the digest cycle to finish
+    $timeout(() => {
+      if (relationalGraph && $scope.isRelationalGraphVisible) {
+        graphHelper.focusOnNodes([$scope.selectedMenuItem.id], relationalGraph);
+        relationalGraph.selection($scope.selectedMenuItem.id);
+        // TODO with the latest version we can zoom on an arbitrary array of elements
+        // the selected entity and the direct neighbours
+        // relationalGraph.zoom('fit', { ids: $scope.selectedMenuItem.id, animate: true});
+        relationalGraph.zoom('selection', { animate: true, time: 600 });
+      }
+    });
+  };
+
+  const hexToRgb = function (hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+  };
+
+  const loadGraph = function (init = true) {
+    if (relationalGraph && $scope.isRelationalGraphVisible) {
+      return ontologyClient.getEntities()
+      .then((entities) => {
+        // add entities
+        const klElements = _.reduce(entities, (total, entity) => {
+          const node = {
+            type: 'node',
+            id: entity.id,
+            d: { entityType: entity.type },
+            t: entity.label,
+            tc: false
+          };
+          if (entity.color && entity.color !== 'undefined') {
+            node.c = entity.color;
+          } else {
+            node.c = '#000000';
+          }
+
+          if (entity.type === 'VIRTUAL_ENTITY') {
+            const rgbColor = hexToRgb(node.c);
+            const rgb = rgbColor.r + ', ' + rgbColor.g + ', ' + rgbColor.b;
+            node.ha0 = {
+              c: 'rgba(' + rgb + ', 0.4)',  //the halo fill color
+              r: 34,                        //the halo radius
+              w: 3                          //the halo width
+            };
+          }
+
+          if (entity.icon && entity.icon !== 'undefined') {
+            node.fi = {
+              c: node.c,
+              t: kl.getFontIcon(entity.icon)
+            }
+            delete node.c;
+          }
+
+          total.push(node);
+          return total;
+        }, []);
+
+        return klElements;
+      })
+      .then((klElements) => {
+        return ontologyClient.getRelations()
+        .then((relations) => {
+          // add relations
+          const excludeInverse = new Set();
+          _.each(relations, (rel) => {
+            if (!excludeInverse.has(rel.inverseOf)) {
+              const link = {
+                type: 'link',
+                t: rel.directLabel,
+                a2: true,
+                id: rel.id,
+                id1: rel.domain.id,
+                id2: rel.range.id,
+                c: 'rgb(0,153,255)',
+                w: 3
+              };
+              klElements.push(link);
+              excludeInverse.add(rel.id);
+            }
+          });
+          return new Promise((fulfill, reject) => {
+            if (init) {
+              relationalGraph.expand(klElements, {layout: {fit: true, tidy: true, tightness: 4}}, () => {
+                fulfill();
+              });
+            } else {
+              relationalGraph.merge(klElements);
+            }
+          });
+        });
+      })
+      .then(() => {
+        updateRelationalGraph();
+      });
+    } else {
+      return Promise.resolve();
+    }
+  };
+
+  $scope.reloadGraph = function (init = true) {
+    return loadGraph(init);
+  };
+
+  $scope.klReady = function (graph) {
+    relationalGraph = graph;
+
+    $scope.$watch(_.throttle(() => {
+      const element =  document.getElementsByClassName('relational-graph');
+      if (element && element[0] && element[0].attributes['ng-show']) {
+        return  {
+          h: element[0].offsetHeight,
+          w: element[0].offsetWidth
+        };
+      } else {
+        return {
+          h: 0,
+          w: 0
+        };
+      }
+    }, 200), (value, oldValue) => {
+      if (relationalGraph && value && oldValue && (value.h !== oldValue.h || value.w !== oldValue.w)) {
+        if (oldValue.w === 0 || oldValue.h === 0) {
+          $scope.reloadGraph(!graphInitialized);
+          graphInitialized = true;
+        }
+        kl.setSize('relationalGraph', value.w, value.h);
+        // updateRelationalGraph();
+      }
+    });
+  };
+
+  $scope.klDblClick = function (id) {
+    if (id && relationalGraph) {
+      const node = relationalGraph.getItem(id);
+      $scope.selectedMenuItem = { id: node.id, type: node.d.entityType };
+    }
+  };
+  //
+  // End of relational graph stuffs
+  //
 });

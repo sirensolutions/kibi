ae07a234c7b05343d016e6af9dda1cc5f4a10690
Colm O'Shea <colmose@gmail.com>
2017-12-21 16:34:51 +0000

    Moved migrate_kibi_yml.js into src.cli.kibi

--- src/cli/kibi/migrate_kibi_yml.js
+++ src/cli/kibi/migrate_kibi_yml.js
@@ -38,7 +38,7 @@ function renamePropAtSpecificPoint(obj, keyToChange, newKeyname) {
 // Take the map of old:new values and convert each config setting in place
 // including nested config options
 // retains the nesting and order of properties
-function migrateKibiYml(path) {
+function migrateKibiYml({ config: path }) {
   let contents = safeLoad(read(path, 'utf8'));
   Object.keys(replacementMap).map(key => {
     function _replaceKeys(obj, oldKey = '', newKey = '') {
@@ -59,11 +59,25 @@ function migrateKibiYml(path) {
 
   const newYml = safeDump(contents);
   // rename kibi.yml to kibi.yml.pre10
-  // rename(fromRoot('config/kibi.yml'), fromRoot('config/kibi.yml.pre10'));
+  // rename(path, `${path}.pre10`);
   // write yaml output as investigate.yml
   write(fromRoot('config/investigate_test.yml'), newYml, { encoding: 'utf8' });
 }
 
-export default {
-  migrateKibiYml
+export default function (program) {
+  async function processCommand(options) {
+    await migrateKibiYml(options);
+  }
+
+  program
+      .command('upgrade-config')
+      .description(
+        'Upgrade configuration settings in the config yaml file'
+      )
+      .option('--dev', 'Run the upgrade using development mode configuration')
+      .option(
+        '-c, --config <path>',
+        'Path to the config file, can be changed with the CONFIG_PATH environment variable as well',
+        process.env.CONFIG_PATH || fromRoot('config/kibi.yml'))
+      .action(processCommand);
 };

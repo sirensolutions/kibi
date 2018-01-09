32e696b9e424f3df22fa95ab5bbc701e3532b737
Fabio <fabio.tacchelli@gmail.com>
2018-01-09 18:24:52 +0100

    Preview the configured instance label

---
+++ src/kibi_plugins/investigate_core/public/management/sections/indices/index_options/helpers/index_options_helper.js
@@ -0,0 +1,130 @@
+export default function IndexOptionsHelperFactory(Promise, es, createNotifier) {
+
+  // Method taken from /query_engine/lib/query_helper.js
+  const parameterizedRegex = /(@doc\[.+?\]@)/g;
+  const _evaluateParameterizedString = function (parameterizedLabel, hit) {
+    let ret = parameterizedLabel;
+    let match = parameterizedRegex.exec(parameterizedLabel);
+
+    while (match !== null) {
+      let group = match[1];
+      group = group.replace('@doc', '');
+      group = group.substring(0, group.length - 1);
+
+      const value = _getValue(hit, group);
+
+      // Escape special characters
+      const reGroup = match[1].replace(/([.*+?^=!:${}()|\[\]\/\\])/g, '\\$1');
+      const re = new RegExp(reGroup, 'g');
+      ret = ret.replace(re, value);
+
+      match = parameterizedRegex.exec(parameterizedLabel);
+    }
+
+    ret = ret.split('\\n').join('\n');
+    return ret;
+  };
+
+  // Method taken from /query_engine/lib/query_helper.js
+  const getValueRegex = /(\[[^\[\]].*?\])/g;
+  const _getValue = function (hit, group) {
+    let value = null;
+
+    let match = getValueRegex.exec(group);
+    let i = 1;
+    while (match !== null) {
+      let propertyName =  match[1];
+      // strip brackets
+      propertyName = propertyName.substring(1, propertyName.length - 1);
+      if (i === 1) {
+        value = hit[propertyName];
+      } else if (!(typeof (value[propertyName]) === 'undefined' || value[propertyName] === null)) {
+        value = value[propertyName];
+      } else {
+        // The property was not found, so we add an empty string
+        value = '';
+      }
+      i++;
+      match = getValueRegex.exec(group);
+    }
+    return value;
+  };
+
+  const notify = createNotifier({
+    location: 'Siren Entity Options Helper'
+  });
+
+  class IndexOptionsHelper {
+    constructor() {
+    }
+
+    getEntityForUpdate(originEntity) {
+      const entity = {
+        id: originEntity.id
+      };
+      if (originEntity.label) {
+        entity.label = originEntity.label;
+      }
+      if (originEntity.type) {
+        entity.type = originEntity.type;
+      }
+      if (originEntity.icon) {
+        entity.icon = originEntity.icon;
+      }
+      if (originEntity.color) {
+        entity.color = originEntity.color;
+      }
+      if (originEntity.shortDescription) {
+        entity.shortDescription = originEntity.shortDescription;
+      }
+      if (originEntity.longDescription) {
+        entity.longDescription = originEntity.longDescription;
+      }
+      if (originEntity.instanceLabel.type) {
+        entity.instanceLabelType = originEntity.instanceLabel.type;
+      }
+      if (originEntity.instanceLabel.value) {
+        entity.instanceLabelValue = originEntity.instanceLabel.value;
+      }
+      return entity;
+    }
+
+    getInstanceLabelPreviewContent(entity) {
+      if (entity.instanceLabel.value) {
+        const request = {
+          index: entity.id,
+          body: {
+            size: 10,
+            query: { match_all: {} }
+          }
+        };
+        return es.search(request)
+        .then((results) => {
+          let html = '';
+          if (results.hits && results.hits.hits) {
+            html += '<table>';
+            html += '  <tr><th>Document id</th><th>Label</th></tr>';
+            _.each(results.hits.hits, (hit) => {
+              html += '  <tr>';
+              html += '    <td>' + hit._id +'</td>';
+
+              if (entity.instanceLabel.type === 'FIELD') {
+                html += '    <td>' + hit._source[entity.instanceLabel.value] + '</td>';
+              } else {
+                html += '    <td>' + _evaluateParameterizedString(entity.instanceLabel.value, hit) + '</td>'
+              }
+              html += '</tr>';
+            });
+            html += '</table>';
+          }
+          return html;
+        })
+        .catch(notify.error);
+      } else {
+        return Promise.resolve('nothing to show');
+      }
+    };
+  };
+
+  return new IndexOptionsHelper();
+};
--- src/kibi_plugins/investigate_core/public/management/sections/indices/index_options/index_options.js
+++ src/kibi_plugins/investigate_core/public/management/sections/indices/index_options/index_options.js
@@ -5,7 +5,7 @@ import './index_options.less';
 
 
 uiModules.get('apps/management')
-.directive('indexOptions', function (ontologyClient, kbnUrl) {
+.directive('indexOptions', function ($compile, Private, ontologyClient, kbnUrl, createNotifier) {
   return {
     restrict: 'E',
     template: indexOptionsTemplate,
@@ -14,38 +14,46 @@ uiModules.get('apps/management')
       save: '='
     },
     link: function ($scope) {
+      const indexOptionsHelpers = Private(require('./helpers/index_options_helper'));
+
+      const addLabelPreviewPopup = function () {
+        var result = document.getElementsByClassName('label-preview');
+        const $el = angular.element(result);
+        $el.qtip('destroy', true);
+        const api = $el.qtip('api');
+        // build html content
+        indexOptionsHelpers.getInstanceLabelPreviewContent($scope.entity)
+        .then((html) =>{
+          if (api) {
+            api.set('content.text', html);
+          } else {
+            $el.qtip({
+              content: {
+                text: html,
+              },
+              position: {
+                my: 'left center',
+                at: 'right center'
+              },
+              hide: {
+                event: 'unfocus click'
+              },
+              show: 'click',
+              style: {
+                classes: 'qtip-light qtip-rounded qtip-shadow entity-label-preview'
+              }
+            });
+          }
+          $compile(html)($scope);
+        })
+      };
+
       $scope.save = function () {
         let promise;
         if ($scope.entity.type === 'VIRTUAL_ENTITY') {
           promise = ontologyClient.updateEntity($scope.entity);
         } else {
-          const entity = {
-            id: $scope.entity.id
-          };
-          if ($scope.entity.label) {
-            entity.label = $scope.entity.label;
-          }
-          if ($scope.entity.type) {
-            entity.type = $scope.entity.type;
-          }
-          if ($scope.entity.icon) {
-            entity.icon = $scope.entity.icon;
-          }
-          if ($scope.entity.color) {
-            entity.color = $scope.entity.color;
-          }
-          if ($scope.entity.shortDescription) {
-            entity.shortDescription = $scope.entity.shortDescription;
-          }
-          if ($scope.entity.longDescription) {
-            entity.longDescription = $scope.entity.longDescription;
-          }
-          if ($scope.entity.instanceLabel.type) {
-            entity.instanceLabelType = $scope.entity.instanceLabel.type;
-          }
-          if ($scope.entity.instanceLabel.value) {
-            entity.instanceLabelValue = $scope.entity.instanceLabel.value;
-          }
+          const entity = indexOptionsHelpers.getEntityForUpdate($scope.entity);
           promise = ontologyClient.updateEntity(entity);
         }
 
@@ -54,6 +62,15 @@ uiModules.get('apps/management')
           kbnUrl.change('/management/siren/entities/' + $scope.entity.id);
         });
       };
+
+      // Init the preview menu
+      if ($scope.entity) {
+        addLabelPreviewPopup();
+      }
+
+      $scope.$watch('entity.instanceLabel.value', () => {
+        addLabelPreviewPopup();
+      })
     }
   };
 });

658482912d8303ab7551ab879277d9e0cea1840a
Darren Gilligan <darren.gilligan@9cookies.com>
2018-01-11 14:59:46 +0000

    getting package name from kibi_version a

--- tasks/gremlin_download.js
+++ tasks/gremlin_download.js
@@ -5,18 +5,19 @@ module.exports = function (grunt) {
 
   grunt.registerTask('getGremlin', function () {
     const done = this.async();
-
     const mvn = maven.create();
+    const version = grunt.config.get('pkg.kibi_version');
+
     mvn.execute('dependency:copy', {
       remoteRepositories: 'https://artifactory.siren.io/artifactory/libs-snapshot-local/',
-      artifact: 'solutions.siren.unipop:gremlin-server:10.0.0-SNAPSHOT',
+      artifact: 'solutions.siren.unipop:gremlin-server:' + version,
       outputDirectory: './gremlin_server',
       'mdep.useBaseVersion': true,
       transitive: false
     }).then(() => {
       // We rename here as command line for maven does not currently support changing name
       // of downloaded artifact: https://issues.apache.org/jira/browse/MDEP-446
-      fs.rename('./gremlin_server/gremlin-server-10.0.0-SNAPSHOT.jar', './gremlin_server/gremlin-server.jar', function (err) {
+      fs.rename('./gremlin_server/gremlin-server-' + version + '.jar', './gremlin_server/gremlin-server.jar', function (err) {
         if (err) {
           done(err);
         }

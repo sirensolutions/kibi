f7b63489f08bc2e5bfd02dee3386a479699cfd43
Darren Gilligan <darren.gilligan@9cookies.com>
2018-01-11 10:35:52 +0000

    add task to download latest gremlin server with npm maven

---
+++ tasks/gremlin_download.js
@@ -0,0 +1,27 @@
+import maven from 'maven';
+import fs from 'fs';
+
+module.exports = function (grunt) {
+
+  grunt.registerTask('getGremlin', function () {
+    const done = this.async();
+
+    const mvn = maven.create();
+    mvn.execute('dependency:copy', {
+      remoteRepositories: 'https://artifactory.siren.io/artifactory/libs-snapshot-local/',
+      artifact: 'solutions.siren.unipop:gremlin-server:10.0.0-SNAPSHOT',
+      outputDirectory: './gremlin_server',
+      'mdep.useBaseVersion': true,
+      transitive: false
+    }).then(() => {
+      // We rename here as command line for maven does not currently support changing name
+      // of downloaded artifact: https://issues.apache.org/jira/browse/MDEP-446
+      fs.rename('./gremlin_server/gremlin-server-10.0.0-SNAPSHOT.jar', './gremlin_server/gremlin-server.jar', function (err) {
+        if (err) {
+          done(err);
+        }
+        done();
+      });
+    }).catch(done);
+  });
+};

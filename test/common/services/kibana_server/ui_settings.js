5ffae48f08b786e4e00355cb807e5aaaef4acb6f
Colm O'Shea <colmose@gmail.com>
2018-01-15 10:34:05 +0000

    Fix config loading into investigate

--- test/common/services/kibana_server/ui_settings.js
+++ test/common/services/kibana_server/ui_settings.js
@@ -12,7 +12,7 @@ export class KibanaServerUiSettings {
     return {
       index: '.kibana',
       type: 'config',
-      id: 'kibi' // kibi: in Kibi the configuration document is always called 'kibi'
+      id: 'siren' // kibi: in Kibi the configuration document is always called 'siren'
     };
   }
 

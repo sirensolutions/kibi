884133165fadb2e9b71fe4f06949cda55924f087
Colm O'Shea <colmose@gmail.com>
2018-01-30 11:59:32 +0000

    Change virtualindices to virtualindexes

--- src/kibi_plugins/investigate_core/public/management/sections/kibi_virtual_indices/index.js
+++ src/kibi_plugins/investigate_core/public/management/sections/kibi_virtual_indices/index.js
@@ -2,8 +2,8 @@ import { management } from 'ui/management';
 import 'plugins/investigate_core/management/sections/kibi_virtual_indices/controllers/virtual_indices_controller';
 import 'plugins/investigate_core/management/sections/kibi_virtual_indices/directives/saved_virtual_indices_finder';
 
-management.getSection('kibana').register('virtualindices', {
-  display: 'Virtual Indices',
+management.getSection('kibana').register('virtualindexes', {
+  display: 'Virtual Indexes',
   order: 2,
-  url: '#/management/siren/virtualindices'
+  url: '#/management/siren/virtualindexes'
 });

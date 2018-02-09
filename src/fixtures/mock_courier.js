6236ebd2f0d190acf60c35d2ba53b00cb874a2bf
Blakko <fabio.tacchelli@gmail.com>
2018-02-09 15:54:45 +0100

    Fix test

--- src/fixtures/mock_courier.js
+++ src/fixtures/mock_courier.js
@@ -1,14 +1,18 @@
 import sinon from 'sinon';
 import { stubbedLogstashIndexPatternService } from 'fixtures/stubbed_logstash_index_pattern';
+import { stubbedSavedObjectIndexPattern } from 'fixtures/stubbed_saved_object_index_pattern';
+import { SavedObjectProvider } from 'ui/courier/saved_object/saved_object';
 
 export default function (Private, Promise) {
   const indexPatterns = Private(stubbedLogstashIndexPatternService);
   const getIndexPatternStub = sinon.stub()
     .returns(Promise.resolve(indexPatterns));
+  const SavedObject = Private(SavedObjectProvider);
 
   const courier = {
     indexPatterns: { get: getIndexPatternStub },
-    getStub: getIndexPatternStub
+    getStub: getIndexPatternStub,
+    SavedObject: SavedObject
   };
 
   return courier;

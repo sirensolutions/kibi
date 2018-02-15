0ce6a977659f343ceecc88c2d12ed5a227a80825
Blakko <fabio.tacchelli@gmail.com>
2018-02-15 14:01:48 +0100

    Add EIDs buttons to update requests

--- src/kibi_plugins/siren_auto_join_vis/public/siren_auto_join_vis_controller.js
+++ src/kibi_plugins/siren_auto_join_vis/public/siren_auto_join_vis_controller.js
@@ -199,8 +199,30 @@ function controller($scope, $rootScope, Private, kbnIndex, config, kibiState, ge
     } else if (edit) {
       return Promise.resolve(buttons);
     }
+    const buttonsToDo = _.reduce(buttons, (total, button) => {
+      if (button.type === 'VIRTUAL_ENTITY') {
+        for (const key in button.sub) {
+          if (button.sub.hasOwnProperty(key)) {
+            _.each(button.sub[key], (btn) => {
+              console.log('add subButton to update count');
+              total.push(btn);
+            });
+          }
+        }
+        for (const key in button.altSub) {
+          if (button.altSub.hasOwnProperty(key)) {
+            _.each(button.altSub[key], (btn) => {
+              total.push(btn);
+            });
+          }
+        }
+      } else {
+        total.push(button);
+      }
+      return total;
+    }, []);
     delayExecutionHelper.addEventData({
-      buttons: buttons,
+      buttons: buttonsToDo,
       dashboardId: dashboardId
     });
     return Promise.resolve(buttons);

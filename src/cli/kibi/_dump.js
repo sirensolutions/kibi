ac4e60fedaf5ce16cad4ffb37423ec3013423f64
Colm O'Shea <colmose@gmail.com>
2018-03-12 21:21:39 +0000

    Delete env var if it was undefined and log if using default ES URL

--- src/cli/kibi/_dump.js
+++ src/cli/kibi/_dump.js
@@ -17,17 +17,30 @@ export default class Dump {
   }
 
   async getElasticsearchURL() {
-    const url = await get(this._config, 'elasticsearch.url', 'http://localhost:9200');
+    let url = get(this._config, 'elasticsearch.url');
+
+    // If no elasticsearch.url set in config, set it to a default
+    if (!url) {
+      const defaultURL = 'http://localhost:9220';
+      console.log(`Using the default Elasticsearch URL (${defaultURL}) as elasticsearch.url is not set`);
+      url = defaultURL;
+    }
+
     if (has(this._config, 'elasticsearch.username') && has(this._config, 'elasticsearch.password')) {
       const [ protocol, ...rest] = url.split('//');
+      // If request is being set over SSL and verificationMode is not set to none, warn the user
+      // that the certificates are not being examined in the TLS handshake
       if (protocol === 'https:') {
         if (this._config.elasticsearch.ssl.verificationMode !== 'none') {
           console.warn('SSL WARNING: The certificates of the cluster will not be verified during this operation');
         }
+
+        // If process.env.NODE_TLS_REJECT_UNAUTHORIZED is not undefined, hold its value temporarily
         if(process.env.NODE_TLS_REJECT_UNAUTHORIZED) {
           this._originalTlsRejectUnauthorizedValue = process.env.NODE_TLS_REJECT_UNAUTHORIZED;
         }
 
+        // Setting this to 0 means the certificates are not used in TLS authentication
         process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
       }
 
@@ -74,6 +87,12 @@ export default class Dump {
     elasticdump.on('error', message => console.error(message));
     await Promise.fromNode(cb => elasticdump.dump(cb));
 
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = this._originalTlsRejectUnauthorizedValue;
+    // once the request is complete, set the env variable back to its original value, if set
+    // or just delete it if it was not set initially
+    if (this._originalTlsRejectUnauthorizedValue !== undefined) {
+      process.env.NODE_TLS_REJECT_UNAUTHORIZED = this._originalTlsRejectUnauthorizedValue;
+    } else {
+      delete process.NODE_TLS_REJECT_UNAUTHORIZED;
+    }
   }
 }
